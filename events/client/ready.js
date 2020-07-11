2a22dbf1d85b71e6ed3999a504a22e3f46a240f1
Luke Hankey <LukeHankey@gmail.com>
2020-07-11 22:46:45 +0100

    Lets see

--- events/client/ready.js
+++ events/client/ready.js
@@ -10,60 +10,61 @@ module.exports = async client => {
 
 	connection.initDb(err => {
 		if (err) console.log(err);
-		let db = getDb();
+	});
+	let db = getDb();
 
-		db.createCollection("Users");
-		const collection = db.collection("Users");
+	db.createCollection("Users");
+	const collection = db.collection("Users");
 
-		function csvJSON(csv) {
+	function csvJSON(csv) {
 
-			const lines = csv.split("\n");
-			const result = [];
-			const headers = lines[0].split(",");
+		const lines = csv.split("\n");
+		const result = [];
+		const headers = lines[0].split(",");
 
-			for(let i = 1;i < lines.length;i++) {
-				const obj = {};
-				const currentline = lines[i].split(",");
+		for(let i = 1;i < lines.length;i++) {
+			const obj = {};
+			const currentline = lines[i].split(",");
 
-				for(let j = 0;j < headers.length;j++) {
-					obj[headers[j]] = currentline[j];
-				}
-
-				result.push(obj);
+			for(let j = 0;j < headers.length;j++) {
+				obj[headers[j]] = currentline[j];
 			}
 
-			// return result; //JavaScript object
-			return JSON.parse(JSON.stringify(result)); // JSON
+			result.push(obj);
 		}
 
-		async function getData() {
-			const clanData = await fetch("http://services.runescape.com/m=clan-hiscores/members_lite.ws?clanName=Valence");
-			const text = clanData.text();
-			const json = text.then(body => csvJSON(body));
+		// return result; //JavaScript object
+		return JSON.parse(JSON.stringify(result)); // JSON
+	}
 
-			json.then(res => {
+	async function getData() {
+		const clanData = await fetch("http://services.runescape.com/m=clan-hiscores/members_lite.ws?clanName=Valence");
+		const text = clanData.text();
+		const json = text.then(body => csvJSON(body));
 
-				const newData = [];
+		json.then(res => {
 
-				for (data of res) {
-					const regex = /�/g;
-					if ((data.Clanmate).includes("�")) {
-						data.Clanmate = data.Clanmate.replace(regex, " ") || data.Clanmate;
-					}
-					newData.push(data);
+			const newData = [];
+
+			for (data of res) {
+				const regex = /�/g;
+				if ((data.Clanmate).includes("�")) {
+					data.Clanmate = data.Clanmate.replace(regex, " ") || data.Clanmate;
 				}
+				newData.push(data);
+			}
 
-				newData.forEach(e => {
-					e._id = e.Clanmate.toUpperCase();
-					// console.log(e);
-				});
-				collection.insertMany(newData);
-			})
-				.catch(error => console.error(error));
-		}
-		collection.updateMany(
-			{},
-			{ $set:
+			newData.forEach(e => {
+				e._id = e.Clanmate.toUpperCase();
+				// console.log(e);
+			});
+			collection.insertMany(newData);
+		})
+			.catch(error => console.error(error));
+	}
+	collection.updateMany(
+		{},
+		{ $set:
 			{ "Caps": 0,
 				"Total Points": 0,
 				"Events": 0,
@@ -75,51 +76,50 @@ module.exports = async client => {
 				"Rank Earned": "",
 				"Donation Points": 0,
 			},
-			},
-			{ upsert: true },
-		);
+		},
+		{ upsert: true },
+	);
 
-		async () => {
-			db = getDb();
-			const vFactsColl = db.collection("Facts");
-			const count = await vFactsColl.stats()
-				.then(res => {
-					return res.count;
-				});
-			const random = Math.floor((Math.random() * count) + 1);
-			vFactsColl.findOne({ number: random })
-				.then(res => {
-					client.channels.cache.get("731324153356877825").send(res.Message);
-				});
+	async () => {
+		db = getDb();
+		const vFactsColl = db.collection("Facts");
+		const count = await vFactsColl.stats()
+			.then(res => {
+				return res.count;
+			});
+		const random = Math.floor((Math.random() * count) + 1);
+		vFactsColl.findOne({ number: random })
+			.then(res => {
+				client.channels.cache.get("731324153356877825").send(res.Message);
+			});
+	};
+	const hour = 60 * 60 * 1000;
+	const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
+	setInterval(async () => {
+		db = getDb();
+		const vFactsColl = db.collection("Facts");
+		const count = await vFactsColl.stats()
+			.then(res => {
+				return res.count;
+			});
+
+		// Change the @Valence Team tag
+		const factEmbed = function(factMessage) {
+			const embed = new Discord.MessageEmbed()
+				.setTitle("**Daily Valence Fact**")
+				.setDescription(factMessage)
+				.setColor(`#${randomColor}`)
+				.addField("**Sent By:**", "<@&685419137530331143>", true)
+				.setTimestamp();
+			return embed;
 		};
-		const hour = 60 * 60 * 1000;
-		const randomColor = Math.floor(Math.random() * 16777215).toString(16);
 
-		setInterval(async () => {
-			db = getDb();
-			const vFactsColl = db.collection("Facts");
-			const count = await vFactsColl.stats()
-				.then(res => {
-					return res.count;
-				});
-
-			// Change the @Valence Team tag
-			const factEmbed = function(factMessage) {
-				const embed = new Discord.MessageEmbed()
-					.setTitle("**Daily Valence Fact**")
-					.setDescription(factMessage)
-					.setColor(`#${randomColor}`)
-					.addField("**Sent By:**", "<@&685419137530331143>", true)
-					.setTimestamp();
-				return embed;
-			};
-
-			const random = Math.floor((Math.random() * count) + 1);
-			vFactsColl.findOne({ number: random })
-				.then(res => {
-					client.channels.cache.get("731324153356877825").send(factEmbed(res.Message));
-					console.log(res.Message);
-				});
-		}, hour);
-	});
+		const random = Math.floor((Math.random() * count) + 1);
+		vFactsColl.findOne({ number: random })
+			.then(res => {
+				client.channels.cache.get("731324153356877825").send(factEmbed(res.Message));
+				console.log(res.Message);
+			});
+	}, hour);
 };
