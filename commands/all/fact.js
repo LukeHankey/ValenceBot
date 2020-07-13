bd87a129cd65cc624d931c0f78b6f57f0e1731f5
Luke Hankey <LukeHankey@gmail.com>
2020-07-13 03:39:33 +0100

    Edit command working

--- commands/all/fact.js
+++ commands/all/fact.js
@@ -31,7 +31,7 @@ module.exports = {
 			return embed;
 		};
 
-		vFactsColl.findOne({ number: random })
+		vFactsColl.findOne({ number: random }) // Fact
 			.then(res => {
 				if (!args[0]) {
 				message.delete();
@@ -41,36 +41,52 @@ module.exports = {
 				}
 			});
 
-			if ((args[0] === "add") && !args[1]) {
+			if ((args[0] === "add") && !args[1]) { // Fact add error
 				console.log("No 2nd argument given.");
 				message.channel.send("Give me a message to add to the DataBase.");
 			}
-			else if ((args[0] === "remove") && !args[1]) {
+			else if ((args[0] === "remove") && !args[1]) { // Fact remove error
 				console.log("No 2nd argument given - remove.");
 				message.channel.send(`You must provide a Fact Number to remove. Use \`${prefix}fact list\` to see the number.`);
 			}
 
-			if (fact && (args[0] === "add")) {
+			if (fact && (args[0] === "add")) { // Fact add
 				await vFactsColl.insertOne({ Message: fact,	number: count+1, })
 					message.delete();
 					message.channel.send(`${code}${count+1}. ${fact}${code} Fact #${count+1} has been added to the list!`);
 					client.channels.cache.get("731997087721586698").send(`<@${message.author.id}> added a Fact: ${code}#${count+1}. ${fact}${code}`);
 				}
 
-			if (fact && (args[0] === "remove")) {
+			if (fact && (args[0] === "remove")) { // Fact remove
 				await vFactsColl.findOne({ number: Number(fact) })
 				.then(res => {
 					if (res.number < count) {
-						vFactsColl.updateMany({ number: { $gt: res.number }}, { $inc: { number: -1 }})
+						vFactsColl.Many({ number: { $gt: res.number }}, { $inc: { number: -1 }})
 						console.log(`Total facts decreased to: ${count-1}`);
 					}
 					message.channel.send(`${code}${res.number}. ${res.Message}${code} Fact #${count} has been deleted from the list!`);
 					client.channels.cache.get("731997087721586698").send(`<@${message.author.id}> removed a Fact: ${code}#${res.number}. ${res.Message}${code}`);
 					});
 				vFactsColl.deleteOne({ number: Number(fact) })
-			}				
+			}
+			
+			if (args[1] && args[2] && (args[0] === "edit")) { // Fact edit
+				const newMessage = args.slice(2).join(" ");
+				if (args[1] !== isNaN) {
+					message.channel.send(`"${args[1]}" is not a valid number!`)
+				}
+				await vFactsColl.findOneAndUpdate({ number: Number(args[1]) }, { $set: { Message: newMessage }})
+				.then(res => {
+					vFactsColl.findOne({ number: res.value.number })
+					.then(r => {
+						message.channel.send(`${code}${res.value.number}. ${res.value.Message} >>> ${r.Message}${code} Fact #${r.number} has been edited successfully!`);
+						client.channels.cache.get("731997087721586698")
+						.send(`<@${message.author.id}> edited Fact #${r.number}: ${code}diff\n- ${res.value.Message}\n+ ${r.Message}${code}`);
+					})
+				})
+			}
 
-			if (args[0] === "list") {
+			if (args[0] === "list") { // Fact list
 				// const listEmbed = function(fNum, fMessage) {
 				// 	const embed = new Discord.MessageEmbed()
 				// 		.setTitle("**Daily Valence Facts**")
@@ -83,7 +99,7 @@ module.exports = {
 				// .forEach(f => message.channel.send(listEmbed(`${f.number}.`, `${f.Message}`)));
 
 				const list = [];
-				await vFactsColl.find({ })
+				await vFactsColl.find({ }).sort({ number: 1})
 				.forEach(x => list.push(`${x.number}. ${x.Message}\n`));
 				// console.log(list.join(" "));
 				await message.channel.send(`${code}${list.slice(0, 10).join("")}${code}`);
