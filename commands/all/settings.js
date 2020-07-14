801ed84fc906f2f226f5007d58a3d2ff71b3e75d
LukeHankey <68163698+LukeHankey@users.noreply.github.com>
2020-07-14 17:35:32 +0100

    switch args

--- commands/all/settings.js
+++ commands/all/settings.js
@@ -22,9 +22,7 @@ module.exports = {
         const db = getDb();
         const collection = db.collection(`Settings`)
 
-	switch(args[0]) {
-		case "":
-			try {
+		if (!args[0]) {
 		    collection.find({}).toArray().then(res => {
 			console.log(Object.keys(res[0]).slice(2, 3).join(""));
 			const key1 = Object.keys(res[0]).slice(2, 3).join("");
@@ -40,43 +38,22 @@ module.exports = {
 			)
 		    )
 		})
-			}
-			catch (err) {
-				console.log(err)
-			}
-		break;
-	}
+	    }
 		
-// 		if (!args[0]) {
-// 		    collection.find({}).toArray().then(res => {
-// 			console.log(Object.keys(res[0]).slice(2, 3).join(""));
-// 			const key1 = Object.keys(res[0]).slice(2, 3).join("");
-// 			const key2 = Object.keys(res[0]).slice(3, 4).join("");
-// 		    message.channel.send(nEmbed(
-// 			"**Settings List**",
-// 			"Here's a list of all the settings you can change:",
-// 			colors.cyan,
-// 			client.user.displayAvatarURL()
-// 		    )
-// 			.addFields(
-// 			    { name: "**Settings**", value: `\`${key1}\`\n\`${key2}\``, inline: false }
-// 			)
-// 		    )
-// 		})
-// 	    }
-		
-		
-        if (args[0] === "prefix" && (!args[1])) {
-            await collection.findOne({ _id: `${message.guild.name}` })
-            .then(res => {
-                message.channel.send(`Your prefix is set as: \`${res.prefix}\``)
-            })
-        }
+		switch (args[0]) {
+			case "prefix":
+// 			if (args[0] === "prefix" && (!args[1])) {
+				await collection.findOne({ _id: `${message.guild.name}` })
+				.then(res => {
+					message.channel.send(`Your prefix is set as: \`${res.prefix}\``)
+				})
+				break;
+			default:
+				console.log("Test");
+// 			}
+		}
 
         if (args[0] === "prefix" && (args[1] === "set") && args[2]) {
-            // await collection.findOne({ _id: message.guild.name })
-            // .then(res => {
-                // console.log(res);
             await collection.findOneAndUpdate({ _id: message.guild.name }, { $set: { prefix: args[2] }}, { returnOriginal: true })
             .then(r => {
                 console.log(r.value);
@@ -84,7 +61,6 @@ module.exports = {
 						client.channels.cache.get("731997087721586698")
 						.send(`<@${message.author.id}> changed the bot Prefix in server: **${message.guild.name}**\n${code}diff\n- ${r.value.prefix}\n+ ${args[2]}${code}`);
             })
-            // })
         }
     },
 };
