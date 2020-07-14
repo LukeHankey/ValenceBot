e402aec1e78e2b683884f30917260340d6e6d36b
LukeHankey <68163698+LukeHankey@users.noreply.github.com>
2020-07-14 17:14:52 +0100

    Trying switch for settings

--- commands/all/settings.js
+++ commands/all/settings.js
@@ -18,28 +18,49 @@ module.exports = {
 			return embed;
 		}; // Discord Embed
     
-		const code = "```";
+	const code = "```";
         const db = getDb();
         const collection = db.collection(`Settings`)
 
-        if (!args[0]) {
-            collection.find({}).toArray().then(res => {
-                console.log(Object.keys(res[0]).slice(2, 3).join(""));
-                const key1 = Object.keys(res[0]).slice(2, 3).join("");
-                const key2 = Object.keys(res[0]).slice(3, 4).join("");
-            message.channel.send(nEmbed(
-                "**Settings List**",
-                "Here's a list of all the settings you can change:",
-                colors.cyan,
-                client.user.displayAvatarURL()
-            )
-                .addFields(
-                    { name: "**Settings**", value: `\`${key1}\`\n\`${key2}\``, inline: false }
-                )
-            )
-        })
-    }
-
+	switch(args[0]) {
+		case: !args[0];
+		    collection.find({}).toArray().then(res => {
+			console.log(Object.keys(res[0]).slice(2, 3).join(""));
+			const key1 = Object.keys(res[0]).slice(2, 3).join("");
+			const key2 = Object.keys(res[0]).slice(3, 4).join("");
+		    message.channel.send(nEmbed(
+			"**Settings List**",
+			"Here's a list of all the settings you can change:",
+			colors.cyan,
+			client.user.displayAvatarURL()
+		    )
+			.addFields(
+			    { name: "**Settings**", value: `\`${key1}\`\n\`${key2}\``, inline: false }
+			)
+		    )
+		})
+		break;
+	}
+		
+// 		if (!args[0]) {
+// 		    collection.find({}).toArray().then(res => {
+// 			console.log(Object.keys(res[0]).slice(2, 3).join(""));
+// 			const key1 = Object.keys(res[0]).slice(2, 3).join("");
+// 			const key2 = Object.keys(res[0]).slice(3, 4).join("");
+// 		    message.channel.send(nEmbed(
+// 			"**Settings List**",
+// 			"Here's a list of all the settings you can change:",
+// 			colors.cyan,
+// 			client.user.displayAvatarURL()
+// 		    )
+// 			.addFields(
+// 			    { name: "**Settings**", value: `\`${key1}\`\n\`${key2}\``, inline: false }
+// 			)
+// 		    )
+// 		})
+// 	    }
+		
+		
         if (args[0] === "prefix" && (!args[1])) {
             await collection.findOne({ _id: `${message.guild.name}` })
             .then(res => {
