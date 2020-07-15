617151a0c1300501a940604bc316839ab93dd085
LukeHankey <68163698+LukeHankey@users.noreply.github.com>
2020-07-15 14:12:47 +0100

    Create send.js

---
+++ commands/all/send.js
@@ -0,0 +1,27 @@
+module.exports = {
+	name: "send",
+	description: ["Sends a message to a channel."],
+	aliases: [""],
+	usage: ["<channel ID> <message content>"],
+	run: async (client, message, args) => {
+  function checkNum(id = 0, gr_eq = 1, l_eq = Infinity) {
+			if (+id !== parseInt(id) || !(id >= gr_eq) || !(id <= l_eq)) {
+				return false
+			} else {
+				return true
+			}
+		}
+    
+    if (!checkNum(args[0], 1, Infinity)) {
+      message.channel.send("Invalid message ID")
+    }
+    
+    let content = args.slice(1).join(" ");
+    
+    if (content) {
+      message.channels.cache.get(args[0]).send(content);
+    }
+  
+  
+    },
+};
