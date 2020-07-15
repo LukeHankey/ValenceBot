40a844fd7dad2778b707ac141d924f252f4cc368
LukeHankey <68163698+LukeHankey@users.noreply.github.com>
2020-07-15 15:06:06 +0100

    Update send.js

--- commands/all/send.js
+++ commands/all/send.js
@@ -4,7 +4,8 @@ module.exports = {
 	aliases: [""],
 	usage: ["<channel ID> <message content>"],
 	run: async (client, message, args) => {
-   	console.log(content);
+	const myID = 212668377586597888;	
+    	let content = args.slice(1).join(" ");
  	function checkNum(id = 0, gr_eq = 1, l_eq = Infinity) {
 		if (+id !== parseInt(id) || !(id >= gr_eq) || !(id <= l_eq)) {
 			return false
@@ -13,16 +14,16 @@ module.exports = {
 		}
 	}
     
-	if (!checkNum(args[0], 1, Infinity)) {
-		message.channel.send("You must provide a channel ID in this server!");
+	if (message.author.id === myID && checkNum(args[0], 1, Infinity)) {
+		message.channel.send("You forgot to include your message content.")
 	}
-	else if (checkNum(args[0], 1, Infinity) && message.guild.channels.cache.has(args[0]) {
+	else if (checkNum(args[0], 1, Infinity) && message.guild.channels.cache.has(args[0])) {
 		 message.channel.send("Provide a message to send.");
 	}
+	else {
+		message.channel.send("You must provide a channel ID");
+	}
 
-    
-    let content = args.slice(1).join(" ");
-		
     if (args[0] && content) {
       client.channels.cache.get(args[0]).send(content);
     }
