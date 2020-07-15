fc945c2d007016b97c892406b6e6441d560566fc
LukeHankey <68163698+LukeHankey@users.noreply.github.com>
2020-07-15 17:06:39 +0100

    Update send.js

--- commands/all/send.js
+++ commands/all/send.js
@@ -15,25 +15,23 @@ module.exports = {
 		}
 	}
 		
-	if (!args[0]) {
+	if (!args[0]) { // No ID
 		message.channel.send("You must provide a channel ID");
 	}
 	
-	if (checkNum(args[0], 1, Infinity) && !args[1]) {
+	if (checkNum(args[0], 1, Infinity) && !args[1]) { // Has ID but no Message
 		 message.channel.send("Provide a message to send.");
 	}
 	
-	if (args[0] && content && message.author.id === myID) {
+	if (checkNum(args[0], 1, Infinity) && content && message.author.id === myID) { // Has ID & content and is bot owner
 		client.channels.cache.get(args[0]).send(content)
 	}
-	else  {
-		if (message.guild.channels.cache.has(args[0])) {
+	else if (message.guild.channels.cache.has(args[0]) && content) {
 		message.guild.channels.cache.get(args[0]).send(content);
-		}
-		else {
+	}
+	else {
 		message.channel.send("You can't send a message to a channel in another server!");
-		}
-	}  
+	}
 		// Allow it to only work in the same server unless it's me
 		// Search through all channels in server, if not found return
     },
