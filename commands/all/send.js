379f3040a268fa8e4e43d6a728d0e21e200b481c
LukeHankey <68163698+LukeHankey@users.noreply.github.com>
2020-07-15 14:55:32 +0100

    Update send.js

--- commands/all/send.js
+++ commands/all/send.js
@@ -4,25 +4,31 @@ module.exports = {
 	aliases: [""],
 	usage: ["<channel ID> <message content>"],
 	run: async (client, message, args) => {
-  function checkNum(id = 0, gr_eq = 1, l_eq = Infinity) {
-			if (+id !== parseInt(id) || !(id >= gr_eq) || !(id <= l_eq)) {
-				return false
-			} else {
-				return true
-			}
+   	console.log(content);
+ 	function checkNum(id = 0, gr_eq = 1, l_eq = Infinity) {
+		if (+id !== parseInt(id) || !(id >= gr_eq) || !(id <= l_eq)) {
+			return false
+		} else {
+			return true
 		}
+	}
     
-    if (!checkNum(args[0], 1, Infinity)) {
-      message.channel.send("Invalid message ID")
-    }
+	if (!checkNum(args[0], 1, Infinity)) {
+		message.channel.send("You must provide a channel ID in this server!");
+	}
+	else if (checkNum(args[0], 1, Infinity) && message.guild.channels.cache.has(args[0]) {
+		 message.channel.send("Provide a message to send.");
+	}
+
     
     let content = args.slice(1).join(" ");
-    console.log(content);
 		
     if (args[0] && content) {
       client.channels.cache.get(args[0]).send(content);
     }
   
+		// Allow it to only work in the same server unless it's me
+		// Search through all channels in server, if not found return
   
     },
 };
