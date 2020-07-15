37941d8b30b246a526935e2780fb9e63b86ba2c1
LukeHankey <68163698+LukeHankey@users.noreply.github.com>
2020-07-15 18:45:14 +0100

    Update send.js

--- commands/all/send.js
+++ commands/all/send.js
@@ -5,8 +5,10 @@ module.exports = {
 	usage: ["<channel ID> <message content>"],
 	run: async (client, message, args) => {
 		
-	const myID = "212668377586597888";	
+	const myID = 212668377586597888;	
     	let content = args.slice(1).join(" ");
+	const code = "```";
+		
  	function checkNum(id = 0, gr_eq = 1, l_eq = Infinity) {
 		if (+id !== parseInt(id) || !(id >= gr_eq) || !(id <= l_eq)) {
 			return false
@@ -24,6 +26,7 @@ module.exports = {
 		}
 		else if (message.author.id !== myID && content && !message.guild.channels.cache.has(args[0])) { // Checks for non-owner, message content and if ID is not in same server
 			message.channel.send("You are not able to send a message to a channel in another server.");
+			client.channels.cache.get("731997087721586698").send(`<@${message.author.id}> tried to send a message to another Server, from Channel: <#${message.channel.id}> to <#{args[0]>: ${code}Server Name: ${message.guild.name}\nServer ID:${message.guild.id}\nMessage content: ${content}${code}`);
 		}
 	}
 	else { // No valid ID
