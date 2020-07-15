67f20c9501967d89d464044b9cb88e9777538766
LukeHankey <68163698+LukeHankey@users.noreply.github.com>
2020-07-15 16:10:01 +0100

    Update eval.js

--- commands/all/eval.js
+++ commands/all/eval.js
@@ -5,20 +5,21 @@ module.exports = {
 	usage:  ["<code>"],
 	run: async (client, message, args) => {
 
-	if (message.author.id !== "212668377586597888") {
-		client.channels.cache.get("731997087721586698").send("<@" + message.author.id + "> tried to use eval!");
-		return message.reply("you wish...")
-		} 
-	else {
-		try {
-			evalCode = eval(args.join(" "));
-			if (typeof evalCode !== "string") {
-				evalCode = require("util").inspect(evalCode)
+		if (message.author.id !== "212668377586597888") {
+			client.channels.cache.get("731997087721586698").send("<@" + message.author.id + "> tried to use eval!");
+			return message.reply("you wish...");
+			} 
+		else {
+			try {
+				evalCode = eval(args.join(" "));
+				if (typeof evalCode !== "string") {
+					evalCode = require("util").inspect(evalCode)
+				}
+			return message.channel.send(evalCode, {code:"xl"})
+			} 
+			catch (error) {
+				return message.channel.send("Error:\n" + error)
 			}
-		return message.channel.send(evalCode, {code:"xl"})
-		} 
-		catch (error) {
-			return message.channel.send("Error:\n" + error)
 		}
-	}
+	},
 }
