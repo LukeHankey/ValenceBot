82f2ad0001af2731f1efc20cc89dfab8080600e6
LukeHankey <68163698+LukeHankey@users.noreply.github.com>
2020-07-16 19:19:20 +0100

    New server owner description added

--- events/guild/roleDelete.js
+++ events/guild/roleDelete.js
@@ -9,15 +9,11 @@ const code = "```";
     .then(res => {
             console.log(res.adminRole)
             if (res.adminRole === `<@&${role.id}>`) {
-                client.users.cache.get(role.guild.ownerID).send(`Your server Admin role was deleted in ${role.guild.name}. Make sure to reset your Admin role to allow your server to use admin commands!`)
-                client.channels.cache.get("731997087721586698").send(`The Admin role **(${role.name})** in ${role.guild.name} has been deleted. The server owner has been sent a message!`);
-                
                 collection.findOneAndUpdate({ adminRole: `<@&${role.id}>` }, { $set: { adminRole: res.defaultAdminRole }})
-				.then(r => {
-                    console.log(r);
-                    console.log(r.adminRole);
-                    console.log(r.defaultAdminRole);
-                })
+		.then(r => {
+			client.users.cache.get(role.guild.ownerID).send(`Your server Admin role was deleted in ${role.guild.name} and has been set back to the default: ${r.value.defaultAdminRole}. Make sure to reset your Admin role to allow your server admins to use admin commands! Use "${r.value.prefix}settings adminRole set <NEW ROLE>" in ${role.guild.name} (Must have the Administrator permission)`)
+			client.channels.cache.get("731997087721586698").send(`The Admin role **(${role.name})** in ${role.guild.name} has been deleted. The server owner has been sent a message!`)
+		;})
             }
         })
 };
