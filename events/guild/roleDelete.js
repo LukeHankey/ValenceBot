93ccf12075bdfb81b9e5bd5697429fe4995956db
Luke Hankey <LukeHankey@gmail.com>
2020-07-16 21:59:33 +0100

    changes from pull

--- events/guild/roleDelete.js
+++ events/guild/roleDelete.js
@@ -4,36 +4,8 @@ module.exports = async (client, role) => {
 let db = getDb();
 const collection = db.collection(`Settings`);
 
-<<<<<<< HEAD
-    collection.findOne({ serverID: role.guild.id })
-||||||| bce0c31
-// let roleName = role.guild.roles;
-    let roleStore = role.guild.roles;
-    console.log(role.guild.ownerID);
-    // console.log(role); // Gets deleted role
-
-    collection.findOne({ _id: role.guild.name })
-=======
     await collection.findOne({ _id: role.guild.name })
->>>>>>> 66709079b38d1e524ce69c3fb1f4f8522dd04d74
     .then(res => {
-<<<<<<< HEAD
-        console.log(res.adminRole)
-        if (res.adminRole === `<@&${role.id}>`) {
-            client.users.cache.get(role.guild.ownerID).send(`Your server Admin role was deleted in ${role.guild.id}. Make sure to reset your Admin role to allow your server to use admin commands!`)
-            client.channels.cache.get("731997087721586698").send(`The Admin role **(${role.id})** in ${role.guild.id} has been deleted. The server owner has been sent a message!`);
-        }
-    })
-};
-||||||| bce0c31
-        console.log(res.adminRole)
-        if (res.adminRole === `<@&${role.id}>`) {
-            client.users.cache.get(role.guild.ownerID).send(`Your server Admin role was deleted in ${role.guild.name}. Make sure to reset your Admin role to allow your server to use admin commands!`)
-            client.channels.cache.get("731997087721586698").send(`The Admin role **(${role.name})** in ${role.guild.name} has been deleted. The server owner has been sent a message!`);
-        }
-    })
-};
-=======
             console.log(res.adminRole)
             if (res.adminRole === `<@&${role.id}>`) {
                 collection.findOneAndUpdate({ adminRole: `<@&${role.id}>` }, { $set: { adminRole: res.defaultAdminRole }})
@@ -44,4 +16,3 @@ const collection = db.collection(`Settings`);
             }
         })
 };
->>>>>>> 66709079b38d1e524ce69c3fb1f4f8522dd04d74
