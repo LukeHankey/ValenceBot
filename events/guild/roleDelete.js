1207622062b37c2a9205242892f4c9d71a07e1b5
LukeHankey <68163698+LukeHankey@users.noreply.github.com>
2020-07-16 18:57:09 +0100

    Finding adminRole fix
    Returned undefined, till try with tags.

--- events/guild/roleDelete.js
+++ events/guild/roleDelete.js
@@ -5,10 +5,6 @@ let db = getDb();
 const collection = db.collection(`Settings`);
 const code = "```";
 
-    let roleStore = role.guild.roles;
-    console.log(role.guild.ownerID);
-    
-       
     await collection.findOne({ _id: role.guild.name })
     .then(res => {
             console.log(res.adminRole)
@@ -16,7 +12,7 @@ const code = "```";
                 client.users.cache.get(role.guild.ownerID).send(`Your server Admin role was deleted in ${role.guild.name}. Make sure to reset your Admin role to allow your server to use admin commands!`)
                 client.channels.cache.get("731997087721586698").send(`The Admin role **(${role.name})** in ${role.guild.name} has been deleted. The server owner has been sent a message!`);
                 
-                collection.findOneAndUpdate({ adminRole: role.id }, { $set: { adminRole: res.defaultAdminRole }})
+                collection.findOneAndUpdate({ adminRole: `<@&${role.id}>` }, { $set: { adminRole: res.defaultAdminRole }})
 				.then(r => {
                     console.log(r);
                     console.log(r.adminRole);
