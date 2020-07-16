dabe89bddf18956ff2744e9b902740bf8383fe11
Luke Hankey <LukeHankey@gmail.com>
2020-07-16 21:43:56 +0100

    pulling

--- events/guild/roleDelete.js
+++ events/guild/roleDelete.js
@@ -3,19 +3,13 @@ const getDb = require("../../mongodb").getDb;
 module.exports = async (client, role) => {
 let db = getDb();
 const collection = db.collection(`Settings`);
-const code = "```";
 
-// let roleName = role.guild.roles;
-    let roleStore = role.guild.roles;
-    console.log(role.guild.ownerID);
-    // console.log(role); // Gets deleted role
-
-    collection.findOne({ _id: role.guild.name })
+    collection.findOne({ serverID: role.guild.id })
     .then(res => {
         console.log(res.adminRole)
         if (res.adminRole === `<@&${role.id}>`) {
-            client.users.cache.get(role.guild.ownerID).send(`Your server Admin role was deleted in ${role.guild.name}. Make sure to reset your Admin role to allow your server to use admin commands!`)
-            client.channels.cache.get("731997087721586698").send(`The Admin role **(${role.name})** in ${role.guild.name} has been deleted. The server owner has been sent a message!`);
+            client.users.cache.get(role.guild.ownerID).send(`Your server Admin role was deleted in ${role.guild.id}. Make sure to reset your Admin role to allow your server to use admin commands!`)
+            client.channels.cache.get("731997087721586698").send(`The Admin role **(${role.id})** in ${role.guild.id} has been deleted. The server owner has been sent a message!`);
         }
     })
 };
