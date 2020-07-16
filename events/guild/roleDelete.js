66709079b38d1e524ce69c3fb1f4f8522dd04d74
LukeHankey <68163698+LukeHankey@users.noreply.github.com>
2020-07-16 19:24:57 +0100

    Code backticks added

--- events/guild/roleDelete.js
+++ events/guild/roleDelete.js
@@ -11,8 +11,8 @@ const code = "```";
             if (res.adminRole === `<@&${role.id}>`) {
                 collection.findOneAndUpdate({ adminRole: `<@&${role.id}>` }, { $set: { adminRole: res.defaultAdminRole }})
 		.then(r => {
-			client.users.cache.get(role.guild.ownerID).send(`Your server Admin role was deleted in ${role.guild.name} and has been set back to the default: ${r.value.defaultAdminRole}. Make sure to reset your Admin role to allow your server admins to use admin commands! Use "${r.value.prefix}settings adminRole set <NEW ROLE>" in ${role.guild.name} (Must have the Administrator permission)`)
-			client.channels.cache.get("731997087721586698").send(`The Admin role **(${role.name})** in ${role.guild.name} has been deleted. The server owner has been sent a message!`)
+			client.users.cache.get(role.guild.ownerID).send(`Your server Admin role was deleted in \`${role.guild.name}\` and has been set back to the default: ${r.value.defaultAdminRole}. Make sure to reset your Admin role to allow your server admins to use admin commands! Use \`${r.value.prefix}settings adminRole set <NEW ROLE>\` in ${role.guild.name} (Must have the Administrator permission)`)
+			client.channels.cache.get("731997087721586698").send(`The Admin role **(${role.name})** in \`${role.guild.name}\` has been deleted. The server owner has been sent a message!`)
 		;})
             }
         })
