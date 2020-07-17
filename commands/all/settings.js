ab908e6616935cf80beefeff2b5563c6de3c7f5a
LukeHankey <68163698+LukeHankey@users.noreply.github.com>
2020-07-17 15:38:03 +0100

    Bug with mention.id fixed

--- commands/all/settings.js
+++ commands/all/settings.js
@@ -85,10 +85,7 @@ module.exports = {
 				case "set":
                     let roleArg = args.slice(2).join(" ");
 					let roleName = message.guild.roles.cache.find(role => role.name === roleArg);
-					let mentionID = message.mentions.roles.first().id;
-					console.log(mentionID);
-					let mentionRole = message.guild.roles.cache.find(role => role.id === mentionID)
-
+					
 					await collection.findOne({ _id: message.guild.name })
     				.then(res => {
 
@@ -152,6 +149,9 @@ module.exports = {
 								}
 						}
 						else if (message.mentions.roles.first()) { // Setting role by mention
+							let mentionID = message.mentions.roles.first().id;
+							console.log(mentionID);
+							let mentionRole = message.guild.roles.cache.find(role => role.id === mentionID)
 							if (mentionRole.rawPosition >= adRole.rawPosition && mentionRole.rawPosition > aboveRP) {
 								message.channel.send("You cannot set the admin role higher than the role you have") // Update to make better message.
 							} 
