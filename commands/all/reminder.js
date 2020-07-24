ed944a3111e04733434af314f1751b2d9ceeac52
LukeHankey <68163698+LukeHankey@users.noreply.github.com>
2020-07-24 16:35:09 +0100

    Update reminder.js

--- commands/all/reminder.js
+++ commands/all/reminder.js
@@ -136,9 +136,10 @@ module.exports = {
 						res.reminders.forEach(x => { idCheck.push(x.id) })
 						console.log(checkNum(args[1], 1, Infinity))
 						console.log(+idCheck.includes(args[1]))
-						if (checkNum(args[1], 1, Infinity) && +idCheck.includes(args[1])) {
+						console.log(idCheck.includes(args[1])
+						if (checkNum(args[1], 1, Infinity) && idCheck.includes(args[1])) {
 							let idMap = [];
-							settings.find({ "reminders.id": args[1] }).map(ids => { idMap.push(ids.id, ids.channel, ids.message) })
+							console.log(settings.find({ "reminders.id": args[1] })) /*.map(ids => { idMap.push(ids.id, ids.channel, ids.message) })*/
 							message.channel.send(`Reminder \`${args[1]}\` has been deleted from <#${idMap[1]}>!\n${code}${args[1]}. ${idMap[2]}${code}`);
 							client.channels.cache.get("731997087721586698").send(`<@${message.author.id}> removed a Reminder: ${code}#${args[1]}. ${idMap[2]}${code}`);
 							settings.updateOne({ _id: message.guild.name }, { $pull: { reminders: { id: args[1] } } })
