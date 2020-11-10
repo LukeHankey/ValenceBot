fb241ad784016dae3f6e9fa8952555b38c945331
Luke Hankey <LukeHankey@gmail.com>
2020-11-10 20:12:14 +0000

    Logging fired only if name has been changed

--- events/guild/guildUpdate.js
+++ events/guild/guildUpdate.js
@@ -1,21 +1,23 @@
 const getDb = require("../../mongodb").getDb;
 
 module.exports = async (client, oldGuild, newGuild) => {
-let db = getDb();
-const collection = db.collection(`Settings`);
-const code = "```";
+    let db = getDb();
+    const collection = db.collection(`Settings`);
+    const code = "```";
 
     collection.updateOne({ _id: `${oldGuild.id}` },
         {
-        $set: { serverName: `${newGuild.name}` },
+            $set: { serverName: `${newGuild.name}` },
         },
         { upsert: true }
-        );
+    );
 
     collection.findOne({ serverID: `${oldGuild.id}` })
-    .then(async res => {
-        client.channels.cache.get("731997087721586698").send(`The server name has been changed from ${oldGuild.name} to **${newGuild.name}**.\n${code}diff\n
+        .then(async res => {
+            if (oldGuild.name !== newGuild.name) {
+                client.channels.cache.get("731997087721586698").send(`The server name has been changed from ${oldGuild.name} to **${newGuild.name}**.\n${code}diff\n
 + Server Name: ${newGuild.name}
 + Server ID: ${oldGuild.id}${code}`);
-})
+            }
+        })
 };
