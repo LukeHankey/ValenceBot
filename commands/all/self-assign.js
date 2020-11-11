47e4923de4b38f4f96f63cc4caa76b859151592f
LukeHankey <68163698+LukeHankey@users.noreply.github.com>
2020-11-11 00:17:03 +0000

    Dsf (#34)
    * Updated help text for SA
    
    * Logging fired only if name has been changed
    
    * Added functionality for csv multi roles

--- commands/all/self-assign.js
+++ commands/all/self-assign.js
@@ -1,41 +1,73 @@
+const colors = require("../../colors.json");
+const { MessageEmbed } = require("discord.js");
+
 module.exports = {
-	name: "self-assign",
-	description: ["Assign a role."],
-	aliases: ["sa"],
-	usage:  ["<role name>"],
-	permissions: [false],
-	run: async (client, message, args, perms) => {
+    name: "self-assign",
+    description: ["Assigns or removes a role."],
+    aliases: ["sa"],
+    usage: ["<role name>"],
+    permissions: [false],
+    run: async (client, message, args, perms) => {
 
         const [...roleName] = args;
 
         const rName = roleName.join(" ").trim();
+        let rNameMulti = rName.split(",")
+        rNameMulti = rNameMulti.map(x => x.trim().toLowerCase())
         const botRole = message.guild.me.roles.cache.find(r => r.managed)
         const highBotRoleID = botRole.id
-        
-        const check = (r, bP) => {
-            const role = message.guild.roles.cache.find(role => role.name.toLowerCase() === r.toLowerCase());
-            if (!role) {
-                return message.channel.send(`Unable to find \`${rName}\`.`)
-            }
-            const roleCheck = message.guild.roles.cache.has(role.id)
-            const botPerms = bP.hasPermission('MANAGE_ROLES')
-            const botRoleHighest = message.guild.roles.cache.get(highBotRoleID);
-            const botHighest = botRoleHighest.position > role.position
 
-            roleCheck ? true : message.channel.send(`Unable to find \`${r ? r : 'blank'}\`. Make sure your spelling is correct.`);
-            botPerms ? true : message.channel.send(`I am missing some permissions: \`MANAGE_ROLES\`.`);
-            botHighest ? true : message.channel.send(`You cannot assign yourself this role.`)
-            
-            return roleCheck && botPerms && botHighest
+        const role = rNameMulti.map(rN => {
+            return message.guild.roles.cache.find(roles => {
+                return roles.name.toLowerCase() === rN
+            })
+        })
+
+        let pos = role.map((x, i) => {
+            if (x === undefined) return i;
+        })
+        pos = pos.filter(item => !isNaN(parseInt(item)))
+        const wrong = pos.map(v => rNameMulti[v])
+
+        if (!args[0]) {
+            return message.channel.send(`Please provide the role name(s) to add/remove.`)
         }
 
-        if (check(rName, message.guild.me)) {
-            const role = message.guild.roles.cache.find(r => r.name.toLowerCase() === rName.toLowerCase())
-            const memberRole = message.member.roles;
-            if (!role) return
-            memberRole.cache.has(role.id)
-            ? memberRole.remove(role.id) && message.channel.send(`${role.name} has been removed.`)
-            : memberRole.add(role.id) && message.channel.send(`${role.name} has been added.`)
-        }
-	},
+        const botRoleHighest = message.guild.roles.cache.get(highBotRoleID);
+        let botHighest = role.filter(x => {
+            if (x === undefined) return
+            return botRoleHighest.position > x.position
+        })
+
+        const rID = botHighest.map(x => x.id)
+        const rNames = botHighest.map(x => x.name)
+
+        const memberRole = message.member.roles;
+        const added = []
+        const removed = [];
+
+        rID.forEach(e => {
+            memberRole.cache.has(e)
+                ? memberRole.remove(rID) && removed.push(rNames)
+                : memberRole.add(rID) && added.push(rNames)
+        })
+
+        const embed = new MessageEmbed()
+            .setTitle('Self-Assigned')
+            .setTimestamp()
+            .setColor(colors.green_light)
+            .setFooter(`${client.user.username} created by Luke_#8346`, message.guild.iconURL())
+
+        const fieldAdd = { name: 'Roles Added:', value: `\`\`\`css\n${!added.length ? "None" : added[0].join(", ")}\`\`\``, inline: true }
+        const fieldRemove = { name: 'Roles Removed:', value: `\`\`\`fix\n${!removed.length ? "None" : removed[0].join(", ")}\`\`\``, inline: true }
+        const wrongAdd = { name: 'Can\'t find:', value: `\`\`\`cs\n'${wrong.join(", ")}'\`\`\``, inline: true }
+        const fields = [fieldAdd, fieldRemove]
+        const fieldsPlus = [...fields, wrongAdd]
+
+        console.log(added, removed, wrong)
+
+        wrong.length && (!added.length && !removed.length) ? message.channel.send(embed.setColor(colors.red_light).addFields(wrongAdd))
+            : wrong.length ? message.channel.send(embed.addFields(fieldsPlus))
+                : message.channel.send(embed.addFields(fields))
+    },
 }
