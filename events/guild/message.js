d7d50f9d026c86de32d9978898f26c2612b73c81
Luke Hankey <LukeHankey@gmail.com>
2020-11-11 05:38:27 +0000

    Added scout tracking. More needed to finish off

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,5 +1,6 @@
 const cron = require('node-cron');
 const { Collection } = require("discord.js");
+const { settings } = require('cluster');
 const getDb = require("../../mongodb").getDb;
 
 module.exports = async (client, message) => {
@@ -95,9 +96,17 @@ module.exports = async (client, message) => {
 
 	// DSF - Merch Calls
 
+	/*
+	* New DB call to store all messages. Data: author.id, timestamp since last post, timestamp readable, count 
+	* Loop through messages, if author.id not found in array, add to array. If found, increase count and change timestamp
+	* 2 roles to reach. 
+	* Command to see who top 10-25 are (all, scouter, verified scouter + staff roles for activity)
+	*/
+
 	await settingsColl.findOne({ _id: message.guild.id })
 		.then(async res => {
 			if (res.merchChannel === undefined) return
+			// if (res.merchChannel === '566338186406789123') return // Remove after
 			if (message.channel.id === await res.merchChannel.channelID) {
 				message.content.match(/(^(?:m|merch|merchant|w|world)+(\s?)(\d{1,3}))/i)
 					? message.channel.send(`<@&670842187461820436>`).then(async m => await m.delete())
@@ -124,18 +133,20 @@ module.exports = async (client, message) => {
 										}
 									}
 								},
-								{ 
+								{
 									sort: { time: 1 },
 									returnNewDocument: true
 								}
 							)
-							.then(async db => {
-								const messageArray = await db.value.merchChannel.messages;
-								if (messageArray[0] === undefined) return;
-								if (messageArray[0].author === "Valence Bot") {
-									await settingsColl.updateOne({ _id: message.guild.id }, { $pull: { "merchChannel.messages": { messageID: messageArray[0].messageID } } })
-								}
-							})
+								.then(async db => {
+									const messageArray = await db.value.merchChannel.messages;
+									if (messageArray[0] === undefined) return;
+									if (messageArray[0].author === "Valence Bot") {
+										await settingsColl.updateOne({ _id: message.guild.id }, { $pull: { "merchChannel.messages": { messageID: messageArray[0].messageID } } })
+									}
+								})
+
+
 						}
 						const count = await settingsColl.findOne({ _id: message.guild.id }).then(async res => {
 							return res.merchChannel.messages.length
@@ -149,7 +160,7 @@ module.exports = async (client, message) => {
 
 								try {
 									const fetched = await message.channel.messages.fetch(lastID)
-									const check = Date.now() - lastTime > 600000
+									const check = Date.now() - lastTime > 60000
 									if (check) {
 										fetched.react('☠️')
 										await settingsColl.updateOne({ _id: message.guild.id }, { $pull: { "merchChannel.messages": { messageID: lastID } } })
@@ -168,6 +179,64 @@ module.exports = async (client, message) => {
 						console.log(err)
 					}
 				})
+				let mes = await message.channel.messages.fetch({ limit: 1 })
+				const log = [...mes.values()]
+				const msg = log.map(val => val)
+
+				if (res.merchChannel.scoutTracker === undefined) return
+				const findMessage = await res.merchChannel.scoutTracker.find(x => x.userID === msg[0].author.id)
+				if (!findMessage) {
+					await settingsColl.findOneAndUpdate({ _id: message.guild.id },
+						{
+							$addToSet: {
+								'merchChannel.scoutTracker': {
+									$each: [{
+										userID: msg[0].author.id,
+										author: msg[0].author.username,
+										firstTimestamp: new Date(msg[0].createdTimestamp),
+										lastTimestamp: msg[0].createdTimestamp,
+										lastTimestampReadable: new Date(msg[0].createdTimestamp),
+										count: 1,
+									}]
+								}
+							}
+						})
+				} else {
+					await settingsColl.updateOne({ _id: message.guild.id, 'merchChannel.scoutTracker.userID': findMessage.userID }, {
+						$inc: {
+							'merchChannel.scoutTracker.$.count': 1,
+						},
+						$set: {
+							'merchChannel.scoutTracker.$.lastTimestamp': msg[0].createdTimestamp,
+							'merchChannel.scoutTracker.$.lastTimestampReadable': new Date(msg[0].createdTimestamp),
+						}
+					})
+				}
+				/* Testing for adding roles - won't work with dsf self assign system
+				* Cron job every hour to check if there are any people who meet the criteria of either role - Check this with 1 minute to see if it continuously posts about the same user
+				* Send to admin channel as a nice embed to let admins know
+				* Create a command as outlined above.
+
+				const scouterRole = message.guild.roles.cache.find(r => r.name === 'Scouter')
+				const scouterRoleV = message.guild.roles.cache.find(r => r.name === 'Verified Scouter')
+				const addRole = (count, role) => {
+					count.forEach(dbs => {
+						message.guild.members.fetch(dbs.userID).then(m => {
+							if (m.roles.cache.has(role.id)) return
+							return m.roles.add(role.id)
+						})
+					})
+				}
+				await settingsColl.findOne({ _id: message.guild.id })
+					.then(async r => {
+						const week = 1000 * 60;
+						const month = 1000 * 60 * 60 * 24 * 31;
+						const counterScout = await r.merchChannel.scoutTracker.filter(val => val.count >= 10 && val.lastTimestamp - val.firstTimestamp >= week)
+						const counterScoutV = await r.merchChannel.scoutTracker.filter(val => val.count >= 25 && val.lastTimestamp - val.firstTimestamp >= month)
+						addRole(counterScout, scouterRole)
+						addRole(counterScoutV, scouterRoleV)
+					})
+					*/
 			}
 		})
 		.catch(err => {
