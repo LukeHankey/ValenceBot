89467438d351a902c6daf4e317eec9c108ed315c
Luke Hankey <LukeHankey@gmail.com>
2020-11-24 01:48:04 +0000

    Add scouter/verified view before weekly post with optional adjustment

--- commands/all/dsf.js
+++ commands/all/dsf.js
@@ -2,6 +2,7 @@ const func = require('../../functions')
 const colors = require('../../colors.json')
 const Discord = require('discord.js')
 const getDb = require('../../mongodb').getDb
+const { ScouterCheck } = require('../../classes.js')
 
 /**
  * 733164313744769024 - Test Server
@@ -11,9 +12,9 @@ const getDb = require('../../mongodb').getDb
 
 module.exports = {
     name: "dsf",
-    description: ['Displays all of the current stored messages.', 'Clears all of the current stored messages.'],
+    description: ['Displays all of the current stored messages.', 'Clears all of the current stored messages.', 'Shows the list of potential scouters/verified scouters with the set scout count or count adjusted.'],
     aliases: [],
-    usage: ['messages|m view', 'messages|m clear'],
+    usage: ['messages|m view', 'messages|m clear', 'view scouter|verified <num (optional)>'],
     guildSpecific: ['733164313744769024', '420803245758480405'],
     run: async (client, message, args, perms) => {
         const db = getDb()
@@ -44,7 +45,7 @@ module.exports = {
                                 return message.channel.send(embed.addFields(fields))
                             })
                         }
-                    break;
+                        break;
                     case 'clear':
                         if (!perms.admin) {
                             return message.channel.send(perms.errorA)
@@ -60,10 +61,58 @@ module.exports = {
                         }
                 }
                 break;
+            case 'view':
+                let scout = new ScouterCheck('Scouter')
+                let vScout = new ScouterCheck('Verified Scouter')
+
+                const classVars = async (name, serverName, db) => {
+                    name._client = client;
+                    name._guild_name = serverName;
+                    name._db = await db.map(doc => {
+                        if (doc.serverName === name._guild_name) return doc
+                    }).filter(x => x)[0]
+                    return name._client && name._guild_name && name._db
+                }
+                const res = await settings.find({}).toArray()
+
+                await classVars(vScout, `Luke's Server`, res)
+                await classVars(scout, `Luke's Server`, res)
+
+                const num = args[2]
+
+                switch (args[1]) {
+                    case 'scouter':
+                        if (num) {
+                            scout = new ScouterCheck('Scouter', parseInt(num))
+                            await classVars(scout, `Luke's Server`, res)
+                            scout.send()
+                        } else {
+                            if (!scout._checkForScouts().length) {
+                                message.channel.send('None found.')
+                            } else return scout.send()
+                        }
+                        break;
+                    case 'verified':
+                        if (num) {
+                            vScout = new ScouterCheck('Verified Scouter', parseInt(num))
+                            await classVars(vScout, `Luke's Server`, res)
+                            vScout.send()
+                        } else {
+                            if (!vScout._checkForScouts().length) {
+                                message.channel.send('None found.')
+                            } else return vScout.send()
+                        }
+                }
+                break;
             default:
+                if (!perms.admin) return message.channel.send(perms.errorA)
+                return message.channel.send(func.nEmbed(
+                    "**DSF List**",
+                    "Here's a list of all the DSF commands you can use:\n\n\`messages|m view\`\n\`messages|m clear\`",
+                    colors.cyan,
+                    client.user.displayAvatarURL()
+                ))
         }
-
-
     }
 
 }
