2ea8f566adff45475b15348e23d2bad89a9ce697
Luke Hankey <LukeHankey@gmail.com>
2020-12-02 01:13:47 +0000

    Adding owner command

---
+++ commands/all/lookup.js
@@ -0,0 +1,44 @@
+const func = require('../../functions')
+const colors = require('../../colors.json')
+const ms = require('pretty-ms')
+const getDb = require("../../mongodb").getDb;
+
+/**
+ * 668330890790699079 - Valence Bot Server
+ * 733164313744769024 - Test Server
+ */
+
+module.exports = {
+    name: "lookup",
+    description: [""],
+    aliases: ["lu"],
+    usage: [""],
+    guildSpecific: ['420803245758480405', '733164313744769024', '668330890790699079'],
+    run: async (client, message, args, perms) => {
+        if (!perms.owner) return message.channel.send(perms.errorO()) 
+        const db = getDb()
+        const settingsColl = db.collection("Settings")
+
+        const val = await settingsColl.findOne({ _id: '420803245758480405' })
+        const fields = []
+        const dataIndex = val.merchChannel.scoutTracker.findIndex(mem => {
+            return mem.userID === args[0]
+        })
+        const member = val.merchChannel.scoutTracker.filter(mem => mem.userID === args[0])
+        val.merchChannel.scoutTracker.filter(mem => {
+            if (mem.userID === args[0]) {
+                const date = (when) => {
+                    let date = new Date(when)
+                    date = date.toString().split(' ')
+                    return date.slice(0, 5).join(' ')
+                }
+                fields.push({ name: `\u200b`, 
+                value: `**firstTimestamp:** ${mem.firstTimestamp}\n**firstTimestampReadable:** ${date(mem.firstTimestampReadable)}\n**lastTimestamp:** ${mem.lastTimestamp}\n**lastTimestampReadable:** ${date(mem.lastTimestampReadable)}\n**Merch count:** ${mem.count}\n**Other count:** ${mem.otherCount}\n**Active for:** ${ms(mem.lastTimestamp - mem.firstTimestamp)}`,
+                inline: true },)
+            }
+        })
+
+        const embed = func.nEmbed(`Diagnostic DB Lookup - ${member[0].author} [${dataIndex}]`, `Testing command to lookup user info for DSF in DB.`, colors.gold)
+        message.channel.send(embed.addFields(fields))
+    }
+}
