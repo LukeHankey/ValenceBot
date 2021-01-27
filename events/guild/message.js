0b147de4c030c4b7f8f24d574e832d1475e74e52
Luke Hankey <LukeHankey@gmail.com>
2021-01-27 06:31:31 +0000

    Auto-calendar update

--- events/guild/message.js
+++ events/guild/message.js
@@ -8,6 +8,13 @@ const { MessageEmbed } = require('discord.js');
 module.exports = async (client, message) => {
 	const db = getDb();
 	const settingsColl = db.collection('Settings');
+	const globalDB = await settingsColl.findOne({ _id: 'Globals' });
+
+	const channels = {
+		vis: globalDB.channels.vis,
+		errors: globalDB.channels.errors,
+		logs: globalDB.channels.logs,
+	};
 
 	// Handling DMs
 
@@ -59,6 +66,8 @@ module.exports = async (client, message) => {
 		.then(async res => {
 			if (res === null) return; // null if merchChannel property doesn't exist
 			// if (res._id === '420803245758480405') return // Remove after
+			if (res.serverName !== 'Luke\'s Server') return;
+
 			const merchID = await res.merchChannel.channelID;
 			const otherID = await res.merchChannel.otherChannelID;
 			const errorLog = [];
@@ -283,7 +292,7 @@ module.exports = async (client, message) => {
 	 *  	❌ - Remove all reactions (Not event post)
 	 * 		✅ - Remove all reactions, add 📌 and ✅:
 	 * 			> Save this message to DB (ID) [{ ID: id }]
-	 * 			> Create a role (@event_name #12345 OR @event_name (event)) (Save role ID to DB) [{ ID: id, roleID: roleID, eventTag: 12345 }]
+	 * 			> Create a role (@event_name #1234 OR @event_name (event)) (Save role ID to DB) [{ ID: id, roleID: roleID, eventTag: 1234 }]
 	 * 			> Add to calendar
 	 * 				> First line of post === event name
 	 * 				> use regex to find date/time
@@ -297,24 +306,100 @@ module.exports = async (client, message) => {
 	 */
 	try {
 		const database = await settingsColl.findOne({ _id: `${message.guild.id}` });
+		if (database.serverName !== 'Luke\'s Server') return;
 		const eventChannel = database.channels.events;
+
+		const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+		const monthIndex = (new Date()).getUTCMonth();
 
 		if (message.channel.id === eventChannel) {
 			const last = message.channel.lastMessage;
+			const eventTitle = last.content.split('\n').map(e => {
+				if (e.includes('*')) {
+					return e.replace(/\*|_/g, '');
+				}
+				return e;
+			});
+			const randomNum = () => (Math.round(Math.random() * 10000) + 1);
 			await last.react('❌');
 			await last.react('✅');
 
 			const filter = (reaction, user) => ['❌', '✅'].includes(reaction.emoji.name) && user.id === message.author.id;
 			const collectOne = await message.awaitReactions(filter, { max: 1, time: 3000, errors: ['time'] });
 			const collectOneReaction = collectOne.first();
+
 			if (collectOneReaction.emoji.name === '❌') {
 				return collectOneReaction.message.reactions.removeAll();
 			}
 			else if (collectOneReaction.emoji.name === '✅') {
-				await settingsColl.updateOne({ _id: message.guild.id }, { });
+				const newRole = await message.guild.roles.create({ data: {
+					name: eventTitle[0].concat(` #${randomNum()}`),
+				} });
+
+				const calChannel = message.guild.channels.cache.find((ch) => ch.name === 'calendar');
+				const dateRegex = /^(Date(:)?\s)+((3[0-1]|2\d|1\d|[1-9])(st|nd|rd|th)?)+\s?(-|to)+\s?((3[0-1]|2\d|1\d|[1-9])(st|nd|rd|th)?)+$/im;
+				const timeRegex = /^(Time(:)?\s)+(([1-6]+(\s)?(day(s)?|week(s)?|month(s)?)$)?|(([0-1]\d|2[0-3]):([0-5]\d)\s)?(-|to)+\s?(([0-1]\d|2[0-3]):([0-5]\d))?)$/im;
+				const link = `https://discord.com/channels/${last.guild.id}/${last.channel.id}/${last.id}`;
+				const thisCal = database.calendarID.filter(prop => (prop.year === new Date().getUTCFullYear()) && prop.month === months[monthIndex]);
+				const m = await calChannel.messages.fetch(thisCal[0].messageID);
+				let dateR, timeR;
+
+				dateRegex.exec(last.content) === null ? dateR = 'null' : dateR = dateRegex.exec(last.content)[0];
+				timeRegex.exec(last.content) === null ? timeR = 'null' : timeR = timeRegex.exec(last.content)[0];
+
+				const addToCal = async (date, time) => {
+					if (date !== 'null') {
+						date[4] === ':' ? date = date.slice(6).trim() : date = date.slice(5).trim();
+					}
+					if (time !== 'null') {
+						console.log(5, time, typeof time);
+						time[4] === ':' ? time = time.slice(6).trim() : time = time.slice(5).trim();
+					}
+					if (time === 'null' || date === 'null') {
+						if (time === 'null' && date === 'null') {
+							client.channels.cache.get(database.channels.adminChannel).send(`${last.author}, there was an error with both the  \`Time\` and \`Date\` parameters and they have been set as null. Please go update the calendar for your event.`);
+						}
+						else {
+							// eslint-disable-next-line no-useless-escape
+							client.channels.cache.get(database.channels.adminChannel).send(`${last.author}, ${time === 'null' ? 'there was an error with the  \`Time\` parameter and it has been set as null. Please go update the calendar for your event.' : 'there was an error with the  \`Date\` parameter and it has been set as null. Please go update the calendar for your event.'}`);
+						}
+					}
+
+					const editEmbed = new MessageEmbed(m.embeds[0]);
+					editEmbed.addFields(
+						{ name: date, value: `Event: ${eventTitle[0]}\nTime: ${time}\n[Announcement](${link})\nHost: ${last.author}` },
+					);
+					m.edit(editEmbed);
+					client.channels.cache.get(channels.logs).send(`Calendar updated - ${message.author} added an event automatically: \`\`\`Date: ${date}, Event: ${eventTitle[0]}, Time: ${time}, Link: ${link}, Host: ${last.author}\`\`\``);
+				};
+
+				if (!dateRegex.test(last.content) || !timeRegex.test(last.content)) {
+					dateRegex.test(last.content) ? addToCal(dateR, timeR) : addToCal(dateR, timeR);
+				}
+				else {
+					addToCal(dateR, timeR);
+				}
+
+				await settingsColl.updateOne({ _id: message.guild.id }, { $push: { events: { $each: [ { messageID: last.id, roleID: newRole.id, eventTag: newRole.name.slice(eventTitle[0].length + 2) } ] } } });
 				await collectOneReaction.message.reactions.removeAll();
 				await last.react('📌');
 				await last.react('✅');
+
+				// const reaction = (r, u) => ['📌', '✅'].includes(r.emoji.name) && u.id === last.author.id;
+				// const collector = last.createReactionCollector(reaction, { time: 1814400000 });
+
+				// collector.on('collect', (r, u) => {
+				// 	if (r.emoji.name === '✅') {
+				// 		if (u.id !== last.author.id) {
+				// 			last.reactions.resolve('✅').users.remove(u.id);
+				// 			return;
+				// 		}
+				// 		r.message.reactions.removeAll();
+				// 		settingsColl.findOneAndUpdate({ _id: message.guild.id }, { $pull: { events: { messageID: last.id } } });
+				// 	}
+				// });
+
+
 			}
 			else {return;}
 		}
@@ -323,11 +408,9 @@ module.exports = async (client, message) => {
 		console.error(err);
 	}
 
-
 	// Commands
 	try {
 		const commandDB = await settingsColl.findOne({ _id: `${message.guild.id}` });
-		const globalDB = await settingsColl.findOne({ _id: 'Globals' });
 		if (!message.content.startsWith(commandDB.prefix)) return;
 
 		const args = message.content.slice(commandDB.prefix.length).split(/ +/g);
@@ -347,12 +430,6 @@ module.exports = async (client, message) => {
 			errorO: owner.ownerError(),
 			errorM: mR.error(),
 			errorA: aR.error(),
-		};
-
-		const channels = {
-			vis: globalDB.channels.vis,
-			errors: globalDB.channels.errors,
-			logs: globalDB.channels.logs,
 		};
 
 		try {
---
+++ events/guild/messageReactionAdd.js
@@ -0,0 +1,22 @@
+const getDb = require('../../mongodb').getDb;
+
+module.exports = async (client, reaction, user) => {
+	const db = getDb();
+	const settingsColl = db.collection('Settings');
+	const message = reaction.message;
+
+	if (message.partial) await message.fetch();
+	const database = await settingsColl.findOne({ _id: `${message.guild.id}` });
+	if (database.events === undefined) return;
+	const data = database.events.filter(m => m.messageID === message.id);
+
+	if (!data.length || user.bot) return;
+	if (user.id !== message.author.id) {
+		message.reactions.resolve('✅').users.remove(user.id);
+		return;
+	}
+	reaction.message.reactions.removeAll();
+	message.guild.roles.fetch(data[0].roleID).then(r => r.delete());
+	settingsColl.findOneAndUpdate({ _id: message.guild.id }, { $pull: { events: { messageID: message.id } } });
+
+};
