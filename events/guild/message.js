d57f462bd4e0d7ae3ab8e2c287289420701b071e
Luke Hankey <LukeHankey@gmail.com>
2021-01-29 15:43:33 +0000

    Added try/catch for errors

--- events/guild/message.js
+++ events/guild/message.js
@@ -303,67 +303,72 @@ module.exports = async (client, message) => {
 				await last.react('❌');
 				await last.react('✅');
 
-				const filter = (reaction, user) => ['❌', '✅'].includes(reaction.emoji.name) && user.id === message.author.id;
-				const collectOne = await message.awaitReactions(filter, { max: 1, time: 30000, errors: ['time'] });
-				const collectOneReaction = collectOne.first();
+				try {
+					const filter = (reaction, user) => ['❌', '✅'].includes(reaction.emoji.name) && user.id === message.author.id;
+					const collectOne = await message.awaitReactions(filter, { max: 1, time: 30000, errors: ['time'] });
+					const collectOneReaction = collectOne.first();
 
-				if (collectOneReaction.emoji.name === '❌') {
-					return collectOneReaction.message.reactions.removeAll();
-				}
-				else if (collectOneReaction.emoji.name === '✅') {
-					const newRole = await message.guild.roles.create({ data: {
-						name: eventTitle[0].concat(` #${randomNum()}`),
-					} });
-
-					const calChannel = message.guild.channels.cache.find((ch) => ch.name === 'calendar');
-					const dateRegex = /^(Date(:)?\s)+((3[0-1]|2\d|1\d|[1-9])(st|nd|rd|th)?)+\s?(-|to)+\s?((3[0-1]|2\d|1\d|[1-9])(st|nd|rd|th)?)+$/im;
-					const timeRegex = /^(Time(:)?\s)+(([1-6]+(\s)?(day(s)?|week(s)?|month(s)?)$)?|(([0-1]\d|2[0-3]):([0-5]\d)\s)?(-|to)+\s?(([0-1]\d|2[0-3]):([0-5]\d))?)$/im;
-					const link = `https://discord.com/channels/${last.guild.id}/${last.channel.id}/${last.id}`;
-					const thisCal = DB.calendarID.filter(prop => (prop.year === new Date().getUTCFullYear()) && prop.month === months[monthIndex]);
-					const m = await calChannel.messages.fetch(thisCal[0].messageID);
-					let dateR, timeR;
-
-					dateRegex.exec(last.content) === null ? dateR = 'null' : dateR = dateRegex.exec(last.content)[0];
-					timeRegex.exec(last.content) === null ? timeR = 'null' : timeR = timeRegex.exec(last.content)[0];
-
-					const addToCal = async (date, time) => {
-						if (date !== 'null') {
-							date[4] === ':' ? date = date.slice(6).trim() : date = date.slice(5).trim();
-						}
-						if (time !== 'null') {
-							time[4] === ':' ? time = time.slice(6).trim() : time = time.slice(5).trim();
-						}
-						if (time === 'null' || date === 'null') {
-							if (time === 'null' && date === 'null') {
-								client.channels.cache.get(DB.channels.adminChannel).send(`${last.author}, there was an error with both the  \`Time\` and \`Date\` parameters and they have been set as null. Please go update the calendar for your event.`);
-							}
-							else {
-							// eslint-disable-next-line no-useless-escape
-								client.channels.cache.get(DB.channels.adminChannel).send(`${last.author}, ${time === 'null' ? 'there was an error with the  \`Time\` parameter and it has been set as null. Please go update the calendar for your event.' : 'there was an error with the  \`Date\` parameter and it has been set as null. Please go update the calendar for your event.'}`);
-							}
-						}
-
-						const editEmbed = new MessageEmbed(m.embeds[0]);
-						editEmbed.addFields(
-							{ name: date, value: `Event: ${eventTitle[0]}\nTime: ${time}\n[Announcement](${link})\nHost: ${last.author}` },
-						);
-						m.edit(editEmbed);
-						client.channels.cache.get(channels.logs).send(`Calendar updated - ${message.author} added an event automatically: \`\`\`Date: ${date}, Event: ${eventTitle[0]}, Time: ${time}, Link: ${link}, Host: ${last.author}\`\`\``);
-					};
-
-					if (!dateRegex.test(last.content) || !timeRegex.test(last.content)) {
-						dateRegex.test(last.content) ? addToCal(dateR, timeR) : addToCal(dateR, timeR);
+					if (collectOneReaction.emoji.name === '❌') {
+						return collectOneReaction.message.reactions.removeAll();
 					}
-					else {
-						addToCal(dateR, timeR);
-					}
+					else if (collectOneReaction.emoji.name === '✅') {
+						const newRole = await message.guild.roles.create({ data: {
+							name: eventTitle[0].concat(` #${randomNum()}`),
+						} });
 
-					await settingsColl.updateOne({ _id: message.guild.id }, { $push: { events: { $each: [ { messageID: last.id, roleID: newRole.id, eventTag: newRole.name.slice(eventTitle[0].length + 2), members: [] } ] } } });
-					await collectOneReaction.message.reactions.removeAll();
-					await last.react('📌');
-					await last.react('✅');
+						const calChannel = message.guild.channels.cache.find((ch) => ch.name === 'calendar');
+						const dateRegex = /^(Date(:)?\s)+((3[0-1]|2\d|1\d|[1-9])(st|nd|rd|th)?)+\s?(-|to)+\s?((3[0-1]|2\d|1\d|[1-9])(st|nd|rd|th)?)+$/im;
+						const timeRegex = /^(Time(:)?\s)+(([1-6]+(\s)?(day(s)?|week(s)?|month(s)?)$)?|(([0-1]\d|2[0-3]):([0-5]\d)\s)?(-|to)+\s?(([0-1]\d|2[0-3]):([0-5]\d))?)$/im;
+						const link = `https://discord.com/channels/${last.guild.id}/${last.channel.id}/${last.id}`;
+						const thisCal = DB.calendarID.filter(prop => (prop.year === new Date().getUTCFullYear()) && prop.month === months[monthIndex]);
+						const m = await calChannel.messages.fetch(thisCal[0].messageID);
+						let dateR, timeR;
+
+						dateRegex.exec(last.content) === null ? dateR = 'null' : dateR = dateRegex.exec(last.content)[0];
+						timeRegex.exec(last.content) === null ? timeR = 'null' : timeR = timeRegex.exec(last.content)[0];
+
+						const addToCal = async (date, time) => {
+							if (date !== 'null') {
+								date[4] === ':' ? date = date.slice(6).trim() : date = date.slice(5).trim();
+							}
+							if (time !== 'null') {
+								time[4] === ':' ? time = time.slice(6).trim() : time = time.slice(5).trim();
+							}
+							if (time === 'null' || date === 'null') {
+								if (time === 'null' && date === 'null') {
+									client.channels.cache.get(DB.channels.adminChannel).send(`${last.author}, there was an error with both the  \`Time\` and \`Date\` parameters and they have been set as null. Please go update the calendar for your event.`);
+								}
+								else {
+									// eslint-disable-next-line no-useless-escape
+									client.channels.cache.get(DB.channels.adminChannel).send(`${last.author}, ${time === 'null' ? 'there was an error with the  \`Time\` parameter and it has been set as null. Please go update the calendar for your event.' : 'there was an error with the  \`Date\` parameter and it has been set as null. Please go update the calendar for your event.'}`);
+								}
+							}
+
+							const editEmbed = new MessageEmbed(m.embeds[0]);
+							editEmbed.addFields(
+								{ name: date, value: `Event: ${eventTitle[0]}\nTime: ${time}\n[Announcement](${link})\nHost: ${last.author}` },
+							);
+							m.edit(editEmbed);
+							client.channels.cache.get(channels.logs).send(`Calendar updated - ${message.author} added an event automatically: \`\`\`Date: ${date}, Event: ${eventTitle[0]}, Time: ${time}, Link: ${link}, Host: ${last.author}\`\`\``);
+						};
+
+						if (!dateRegex.test(last.content) || !timeRegex.test(last.content)) {
+							dateRegex.test(last.content) ? addToCal(dateR, timeR) : addToCal(dateR, timeR);
+						}
+						else {
+							addToCal(dateR, timeR);
+						}
+
+						await settingsColl.updateOne({ _id: message.guild.id }, { $push: { events: { $each: [ { messageID: last.id, roleID: newRole.id, eventTag: newRole.name.slice(eventTitle[0].length + 2), members: [] } ] } } });
+						await collectOneReaction.message.reactions.removeAll();
+						await last.react('📌');
+						await last.react('✅');
+					}
+					else {return;}
 				}
-				else {return;}
+				catch (err) {
+					console.log(err);
+				}
 			}
 		});
 
