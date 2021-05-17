000b7a6e2de486ece3a6150b0f0a24429fbc3dc2
Luke Hankey <LukeHankey@gmail.com>
2021-05-17 05:48:04 +0100

    Took a function from other file to make global use

--- functions.js
+++ functions.js
@@ -82,4 +82,34 @@ module.exports = {
 		};
 		return await removeMessages();
 	},
+	compressArray: function(original) {
+
+		const compressed = [];
+		// make a copy of the input array
+		const copy = original.slice(0);
+
+		// first loop goes over every element
+		for (let i = 0; i < original.length; i++) {
+
+			let myCount = 0;
+			// loop over every element in the copy and see if it's the same
+			for (let w = 0; w < copy.length; w++) {
+				if (original[i] == copy[w]) {
+					// increase amount of times duplicate is found
+					myCount++;
+					// sets item to undefined
+					delete copy[w].id;
+				}
+			}
+
+			if (myCount > 0) {
+				const a = new Object();
+				a.value = original[i];
+				a.count = myCount;
+				compressed.push(a);
+			}
+		}
+
+		return compressed;
+	},
 };
