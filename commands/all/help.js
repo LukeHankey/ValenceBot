0b537f83ce87220465640490cd16617241f355c6
Luke Hankey <LukeHankey@gmail.com>
2021-06-09 01:17:42 +0100

    Reworked help command to only show command per permission level of member using command

--- commands/all/help.js
+++ commands/all/help.js
@@ -8,64 +8,81 @@ module.exports = {
 	aliases: ['commands'],
 	usage: ['command name'],
 	guildSpecific: 'all',
-	run: async (client, message, args) => {
+	permissionLevel: 'Everyone',
+	run: async (client, message, args, perms) => {
 		const { commands } = message.client;
 		const db = getDb();
 		const settings = db.collection('Settings');
+		const { prefix } = await settings.findOne({ _id: message.guild.id }, { projection: { prefix: 1 } });
 
-		settings.findOne({ _id: message.guild.id })
-			.then(res => {
-				if (!args.length) {
-					const com = commands.map(command => {
-						if (command.guildSpecific.includes(message.guild.id) || command.guildSpecific === 'all') {
-							return `\`${command.name}\``;
+		if (!args.length) {
+			const com = commands.map(command => {
+				if (command.guildSpecific.includes(message.guild.id) || command.guildSpecific === 'all') {
+					switch (perms) {
+					default:
+						if (perms.owner) {
+							if (command.permissionLevel === 'Owner' || command.permissionLevel === 'Admin' || command.permissionLevel === 'Mod' || command.permissionLevel === 'Everyone') return `\`${command.name}\``;
 						}
-					});
-					const join = com.filter(x => x).join('|');
-
-					message.channel.send(func.nEmbed(
-						'**Help Commands List**',
-						'Here\'s a list of all my commands:',
-						colors.cyan,
-						message.author.displayAvatarURL(),
-						client.user.displayAvatarURL(),
-					)
-						.addFields(
-							{ name: '**Commands:**', value: join, inline: false },
-							{ name: `**The bot prefix is: ${res.prefix}**`, value: `\nYou can send \`${res.prefix}help [command name]\` to get info on a specific command!`, inline: false },
-						),
-					);
-				}
-				else {
-					const name = args[0];
-					const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
-					const otherView = ['Lotto', 'Calendar', 'Profile', 'Dsf'];
-
-					const cName = func.capitalise(command.name);
-					const fields = [];
-
-					for (let i = 0; i < command.usage.length; i++) {
-						if (otherView.includes(cName)) {
-							const field = { name: `🔹 ${res.prefix}${cName} ${command.usage[i] || ''}`, value: `${command.description[i]}`, inline: false };
-							fields.push(field);
+						else if (perms.admin) {
+							if (command.permissionLevel === 'Admin' || command.permissionLevel === 'Mod' || command.permissionLevel === 'Everyone') return `\`${command.name}\``;
 						}
-						else {
-							const field = { name: `🔹 ${res.prefix}${cName} ${command.usage[i] || ''}`, value: `${command.description[i]}`, inline: true };
-							fields.push(field);
+						else if (perms.mod) {
+							if (command.permissionLevel === 'Mod' || command.permissionLevel === 'Everyone') return `\`${command.name}\``;
 						}
-					// console.log(field);
+						else if (command.permissionLevel === 'Everyone') {return `\`${command.name}\``;}
+						else { return;}
 					}
-
-					message.channel.send(func.nEmbed(
-						`**Command:** ${cName}`,
-						`**Aliases:** ${command.aliases.join(', ') || '[NO ALIASES]'}\n**Usage:**`,
-						colors.aqua,
-						message.member.user.displayAvatarURL(),
-						client.user.displayAvatarURL(),
-					)
-						.addFields(fields),
-					);
 				}
 			});
+			const join = com.filter(x => x).join('|');
+
+
+			/**
+			 * Check if permission level of the user using the command is equal to that of the permission level for each command per guild.
+			 */
+
+
+			message.channel.send(func.nEmbed(
+				'**Help Commands List**',
+				'Here\'s a list of all my commands:',
+				colors.cyan,
+				message.author.displayAvatarURL(),
+				client.user.displayAvatarURL(),
+			)
+				.addFields(
+					{ name: '**Commands:**', value: join, inline: false },
+					{ name: `**The bot prefix is: ${prefix}**`, value: `\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`, inline: false },
+				),
+			);
+		}
+		else {
+			const name = args[0];
+			const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+			const otherView = ['Lotto', 'Calendar', 'Profile', 'Dsf'];
+
+			const cName = func.capitalise(command.name);
+			const fields = [];
+
+			for (let i = 0; i < command.usage.length; i++) {
+				if (otherView.includes(cName)) {
+					const field = { name: `🔹 ${prefix}${cName} ${command.usage[i] || ''}`, value: `${command.description[i]}`, inline: false };
+					fields.push(field);
+				}
+				else {
+					const field = { name: `🔹 ${prefix}${cName} ${command.usage[i] || ''}`, value: `${command.description[i]}`, inline: true };
+					fields.push(field);
+				}
+			}
+
+			message.channel.send(func.nEmbed(
+				`**Command:** ${cName}`,
+				`**Aliases:** ${command.aliases.join(', ') || '[NO ALIASES]'}\n**Permission Level:** ${command.permissionLevel}\n**Usage:**`,
+				colors.aqua,
+				message.member.user.displayAvatarURL(),
+				client.user.displayAvatarURL(),
+			)
+				.addFields(fields),
+			);
+		}
 	},
 };
