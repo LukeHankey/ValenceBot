f713323d4a21348aa1cc7360e85c8bd139ad6199
Luke Hankey <LukeHankey@gmail.com>
2021-07-01 23:52:57 +0100

    Removing channel overrides when marked as dead by bot

--- dsf/merchChannel/skullTimer.js
+++ dsf/merchChannel/skullTimer.js
@@ -3,23 +3,22 @@ const cron = require('node-cron');
 const skullTimer = (message, updateDB) => {
 // Checking the DB and marking dead calls
 	const timer = cron.schedule('* * * * *', async () => {
-		const data = await updateDB.findOne({ _id: message.guild.id });
-		const messagesDB = data.merchChannel.messages;
-		for await (const doc of messagesDB) {
-			const lastID = doc.messageID;
-			const lastTime = doc.time;
+		const { merchChannel: { messages, channelID } } = await updateDB.findOne({ _id: message.guild.id }, { projection: { 'merchChannel.messages': 1, 'merchChannel.channelID': 1 } });
+		const merchChannelID = message.guild.channels.cache.get(channelID);
+		for await (const { messageID, content, time, userID } of messages) {
 
 			try {
 				// Removes bot messages
-				if (doc.userID === '668330399033851924' || doc.content.includes('<@&670842187461820436>')) {
-					await updateDB.updateOne({ _id: message.guild.id }, { $pull: { 'merchChannel.messages': { messageID: doc.messageID } } });
+				if (userID === '668330399033851924' || content.includes('<@&670842187461820436>')) {
+					await updateDB.updateOne({ _id: message.guild.id }, { $pull: { 'merchChannel.messages': { messageID: messageID } } });
 				}
 
-				if (Date.now() - lastTime > 600000) {
-					const fetched = await message.channel.messages.fetch(lastID);
+				if (Date.now() - time > 600000) {
+					const fetched = await message.channel.messages.fetch(messageID);
 					fetched.react('☠️')
 						.then(async () => {
-							await updateDB.updateOne({ _id: message.guild.id }, { $pull: { 'merchChannel.messages': { messageID: lastID } } });
+							await merchChannelID.permissionOverwrites.get(userID).delete();
+							return await updateDB.updateOne({ _id: message.guild.id }, { $pull: { 'merchChannel.messages': { messageID: messageID } } });
 						})
 						.catch(() => {
 							return timer.stop();
@@ -37,14 +36,14 @@ const skullTimer = (message, updateDB) => {
 
 		// Removing duplicates
 		const counts = {};
-		messagesDB.forEach(function(x) { counts[x.messageID] = (counts[x.messageID] || 0) + 1; });
+		messages.forEach(function(x) { counts[x.messageID] = (counts[x.messageID] || 0) + 1; });
 		function getKeyByValue(object, value) {
 			return Object.keys(object).find(key => object[key] === value);
 		}
 		Object.values(counts).forEach(dupe => {
 			if (dupe > 1) {
 				const message_id = getKeyByValue(counts, dupe);
-				const entry = messagesDB.find(id => id.messageID === message_id);
+				const entry = messages.find(id => id.messageID === message_id);
 				updateDB.updateOne({ _id: message.guild.id }, { $pull: { 'merchChannel.messages': { messageID: message_id } } });
 				return updateDB.updateOne({ _id: message.guild.id }, { $addToSet: { 'merchChannel.messages': entry } });
 			}
