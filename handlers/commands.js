b9263152d47b8e12acafbe5e989bb25fb1db8b35
Luke Hankey <LukeHankey@gmail.com>
2021-07-07 23:16:17 +0100

    eslint

--- handlers/commands.js
+++ handlers/commands.js
@@ -1,16 +1,14 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
-const { readdirSync } = require("fs");
+const { readdirSync } = require('fs');
 
 module.exports = client => {
 	const load = dirs => {
 		const commandFiles = readdirSync(`./commands/${dirs}/`)
-			.filter(d => d.endsWith(".js"));
+			.filter(d => d.endsWith('.js'));
 
 		for (const file of commandFiles) {
 			const command = require(`../commands/${dirs}/${ file }`);
 			client.commands.set(command.name, command);
 		}
 	};
-	["all"].forEach(x => load(x));
+	['all'].forEach(x => load(x));
 };
--- handlers/events.js
+++ handlers/events.js
@@ -1,17 +1,15 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
-const { readdirSync } = require("fs");
+const { readdirSync } = require('fs');
 
 module.exports = (client) => {
 	const load = dirs => {
 		const events = readdirSync(`./events/${dirs}/`)
-			.filter(d => d.endsWith(".js"));
+			.filter(d => d.endsWith('.js'));
 
 		for (const file of events) {
 			const evt = require(`../events/${dirs}/${file}`);
-			const name = file.split(".")[0];
+			const name = file.split('.')[0];
 			client.on(name, evt.bind(null, client));
 		}
 	};
-	["client", "guild"].forEach(x => load(x));
+	['client', 'guild'].forEach(x => load(x));
 };
