13877e40e9dc0e6b3508dba0c32da6dccaf19f88
LukeHankey <68163698+LukeHankey@users.noreply.github.com>
2021-08-02 23:55:50 +0100

    Switched around current and previous name for better reading

--- events/client/ready.js
+++ events/client/ready.js
@@ -71,8 +71,8 @@ module.exports = async client => {
 		dataChanged = dataChanged.map((profile) => {
 			return `${padding(profile.clanMate, true, Math.max(...(dataChanged.map(el => el.clanMate.length))))}${padding(profile.clanRank, false, Math.max(...(dataChanged.map(el => el.clanRank.length))))}${padding(profile.totalXP, false, Math.max(...(dataChanged.map(el => el.totalXP.length))))}${padding(profile.kills, false, Math.max(...(dataChanged.map(el => el.kills.length))))}`;
 		});
-		dataChanged.splice(0, 0, `These are potential previous names for ${data[0].clanMate}.\n`);
-		dataChanged.push(' ', 'Reactions:\n✅ Takes the primary suggestion suggestion.\n❌ Not changed names or none match.\n📝 Pick another suggestion.');
+		dataChanged.splice(0, 0, `'${data[0].clanMate}' might have changed names to one of these potential new names.\n`);
+		dataChanged.push(' ', 'Reactions:\n✅ Takes the primary suggestion.\n❌ Not changed names or none match.\n📝 Pick another suggestion.');
 		return dataChanged.join('\n');
 	};
 
