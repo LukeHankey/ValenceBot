a2958d9c8f423ae0073dd598f47ae650d75d9579
LukeHankey <68163698+LukeHankey@users.noreply.github.com>
2021-08-08 13:09:53 +0100

    Check if they already marked as inactive

--- dsf/scouts/scouters.js
+++ dsf/scouts/scouters.js
@@ -51,7 +51,7 @@ const removeInactives = async (name, settings, { logs }) => {
 		}
 		else if (doc.active === 0) {
 			const timeStamp = doc.lastTimestampReadable.toString().split(' ');
-			allItems.push(`${doc.author} - ${doc.userID} (${doc.count + doc.otherCount} - M${doc.count}). Last made a call on ${timeStamp.slice(0, 5).join(' ')}.`);
+			allItems.push(`${doc.author} - ${doc.userID} (${doc.count + doc.otherCount} - M${doc.count}). ${timeStamp.slice(0, 5).join(' ')}.`);
 			return;
 		}
 		else {
