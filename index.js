358ece610752c33eafc012b8f7521e41df2a1cb4
LukeHankey <68163698+LukeHankey@users.noreply.github.com>
2021-08-13 14:03:10 +0100

    Removed one intent and logging

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Client, Collection } = require('discord.js');
 const connection = require('./mongodb').initDb;
 require('dotenv').config();
 
-const client = new Client({ intents: [ 'GUILDS', 'GUILD_MEMBERS', 'GUILD_BANS', 'GUILD_INTEGRATIONS', 'GUILD_WEBHOOKS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS', 'DIRECT_MESSAGES'], partials: ['MESSAGE', 'REACTION', 'CHANNEL'] });
+const client = new Client({ intents: [ 'GUILDS', 'GUILD_MEMBERS', 'GUILD_INTEGRATIONS', 'GUILD_WEBHOOKS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS', 'DIRECT_MESSAGES'], partials: ['MESSAGE', 'REACTION', 'CHANNEL'] });
 client.commands = new Collection();
 
 ['commands', 'events'].forEach(x => require(`./handlers/${x}`)(client));
@@ -12,6 +12,5 @@ process.on('unhandledRejection', (reason, p) => {
 });
 
 connection(err => { if (err) console.log(err);});
-console.log(process.env.NODE_ENV);
 
 client.login(process.env.BOT_TOKEN);
