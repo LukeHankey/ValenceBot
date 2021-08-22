a8b5c63edd61cbedf74760dc4a6db2676566ca3a
Luke Hankey <68163698+LukeHankey@users.noreply.github.com>
2021-08-22 23:00:17 +0100

    Install/Update packages

--- commands/all/lotto.js
+++ commands/all/lotto.js
@@ -5,7 +5,6 @@ import { greenLight, redDark, gold, greenDark, redLight } from '../../colors.js'
 import { getDb } from '../../mongodb.js'
 
 /**
- * 733164313744769024 - Test Server
  * 668330890790699079 - Valence Bot Test
  * 472448603642920973 - Valence
  */
