49704f8dcf70653760c941ff4e49e6a126df9b5b
Luke Hankey <68163698+LukeHankey@users.noreply.github.com>
2021-08-23 01:37:44 +0100

    Other calls fix

--- dsf/merch/main.js
+++ dsf/merch/main.js
@@ -27,7 +27,7 @@ const dsf = async (client, message, channels) => {
 		skullTimer(message, settingsColl, channels);
 	}
 	else if (message.channel.id === otherChannelID) {
-		if (arrIncludesString(disallowedWords, message.content)) {
+		if (!arrIncludesString(disallowedWords, message.content)) {
 			return message.delete();
 		}
 		await otherCalls(message, settingsColl, channels);
