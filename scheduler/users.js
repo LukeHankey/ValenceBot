03326e12e0ae73af28c0c18cc95fde1202b6c9e1
Luke Hankey <68163698+LukeHankey@users.noreply.github.com>
2021-10-11 18:46:48 +0100

    Refactor clan leavers

--- scheduler/users.js
+++ scheduler/users.js
@@ -42,33 +42,28 @@ const addActive = async () => {
 const clanCheck = async (users) => {
 	const db = await getDb();
 	const usersColl = db.collection('Users');
-	let index = 0;
-	const interval = setInterval(async () => {
-		try {
-			let metricsProfile = await fetch(`https://secure.runescape.com/m=website-data/playerDetails.ws?names=%5B%22${users[index].clanMate}%22%5D&callback=jQuery000000000000000_0000000000&_=0`).then(response => response.text());
-			metricsProfile = JSON.parse(metricsProfile.slice(34, -4));
+	try {
+		let metricsProfile = await fetch(`https://secure.runescape.com/m=website-data/playerDetails.ws?names=%5B%22${users}%22%5D&callback=jQuery000000000000000_0000000000&_=0`).then(response => response.text());
+		metricsProfile = JSON.parse(metricsProfile.slice(34, -4));
 
-			if (metricsProfile.clan && metricsProfile.clan !== 'Valence') {
-				// console.log(metricsProfile.name, 'has left Valence for', metricsProfile.clan);
-				await usersColl.deleteOne({ clanMate: metricsProfile.name }, { justOne: true });
-			}
-			else if (metricsProfile.clan && metricsProfile.clan === 'Valence') {
-				console.log(metricsProfile.name, 'still in Valence');
-			}
-			else {
-				console.log(metricsProfile.name, 'is not in any clan');
-				await usersColl.deleteOne({ clanMate: metricsProfile.name }, { justOne: true });
-			}
-			index++;
+		if (metricsProfile.clan && metricsProfile.clan !== 'Valence') {
+			console.log(metricsProfile.name, 'has left Valence for', metricsProfile.clan);
+			await usersColl.deleteOne({ clanMate: metricsProfile.name }, { justOne: true });
+			return false;
 		}
-		catch (err) {
-			console.error(err);
+		else if (metricsProfile.clan && metricsProfile.clan === 'Valence') {
+			console.log(metricsProfile.name, 'still in Valence');
+			return true;
 		}
-
-		if (index === users.length) {
-			clearInterval(interval);
+		else {
+			console.log(metricsProfile.name, 'is not in any clan');
+			await usersColl.deleteOne({ clanMate: metricsProfile.name }, { justOne: true });
+			return false;
 		}
-	}, 1000);
+	}
+	catch (err) {
+		console.error(err);
+	}
 };
 
 const nameChanges = async (missingNames) => {
@@ -76,30 +71,31 @@ const nameChanges = async (missingNames) => {
 	const usersColl = db.collection('Users');
 
 	const nameChange = {
-		left: [],
 		change: [],
 	};
 
 	for (const names of missingNames) {
 		const potentialChangers = await usersColl.findOne({ clanMate: names });
 		const potentialNewNames = await usersColl.find({ clanRank: potentialChangers.clanRank, kills: potentialChangers.kills, gameActive: 'undefined' }).toArray();
-		if (!potentialNewNames.length) {
-			nameChange.left.push(potentialChangers);
-		}
-		else {
-			const xpCheck = potentialNewNames.filter(user => {
-				if (Number(user.totalXP) - 10000000 < Number(potentialChangers.totalXP) && Number(user.totalXP) + 10000000 > Number(potentialChangers.totalXP)) {
-					return user;
-				}
-			});
-			potentialChangers.potentialNewNames = xpCheck;
-			nameChange.change.push(potentialChangers);
-		}
+		let counter = 0;
+		const interval = setInterval(async () => {
+			if (missingNames.length === counter) return clearInterval(interval);
+			const check = await clanCheck(names);
+
+			if (check) {
+				const xpCheck = potentialNewNames.filter(user => {
+					if (Number(user.totalXP) - 10000000 < Number(potentialChangers.totalXP) && Number(user.totalXP) + 10000000 > Number(potentialChangers.totalXP)) {
+						return user;
+					}
+				});
+				potentialChangers.potentialNewNames = xpCheck;
+				nameChange.change.push(potentialChangers);
+			}
+			counter++;
+		}, 1000);
+
 	}
 
-	if (nameChange.left.length) {
-		return await clanCheck(nameChange.left);
-	}
 	if (nameChange.change.length) {
 		return nameChange.change.forEach(async user => {
 			console.log(`Updating ${user.clanMate} as they have potentially changed names`);
