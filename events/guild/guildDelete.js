9d52a2769fa0ced553143d0a11493414041a4002
Luke Hankey <68163698+LukeHankey@users.noreply.github.com>
2021-11-26 03:19:07 +0000

    Fix guild owner await

--- events/guild/guildDelete.js
+++ events/guild/guildDelete.js
@@ -5,10 +5,12 @@ export default async (client, guild) => {
 	const channels = await db.channels
 	try {
 		await db.collection.deleteOne({ _id: `${guild.id}` })
+		const owner = await guild.fetchOwner()
+
 		channels.logs.send(`The bot has left **${guild.name}**. The bot is in a total of ${client.guilds.cache.size} servers.
     \n\`\`\`diff\n+ Server name: ${guild.name}
 + Server ID: ${guild.id}
-+ Owner: ${await guild.fetchOwner().nickname}
++ Owner: ${owner.nickname}
 + Channel count: ${guild.channels.cache.size}
 + Member count: ${guild.memberCount}\`\`\``)
 	} catch (err) {
