4c80c3aee82a6009e7147a3ada6ca5d43776a65c
Luke Hankey <68163698+LukeHankey@users.noreply.github.com>
2021-11-29 17:33:05 +0000

    More flexible select menus

--- handlers/interactions/selectMenu.js
+++ handlers/interactions/selectMenu.js
@@ -1,37 +1,26 @@
-export const selectMenu = async (interaction, _, db) => {
+export const selectMenu = async (interaction, _, db, cache) => {
 	const client = interaction.client
 	const channels = await db.channels
 
 	if (interaction.guild === null) {
 		// DSF Specific
-		const serverName = interaction.message.content.split('\n')[0]
-		const dmData = await db.collection.findOne({ serverName }, { projection: { merchChannel: { components: 1, deletions: 1 } } })
-		const thisSelection = dmData.merchChannel.components.filter(b => {
-			return b.selectMessageID === interaction.message.id
-		})
-		if (interaction.customId === thisSelection[0].selectID) {
+		/**
+		 * customId = `DM ${interaction.user.username}`
+		 */
+		if (interaction.customId === `DM ${interaction.user.username}`) {
 			try {
 				await interaction.update({ components: [] })
 				const errorChannel = client.channels.cache.get('794608385106509824')
-				const buttonMessage = await errorChannel.messages.fetch(thisSelection[0].buttonMessageID)
+				const buttonMessage = await errorChannel.messages.fetch(cache.firstKey())
 				if (interaction.values.includes('yes')) {
 					await interaction.followUp({ content: 'Thank you for responding, the log has been automatically removed.' })
 					await buttonMessage.delete()
-					errorChannel.send({ content: `A password was confirmed by <@!${thisSelection[0].userID}> and the message has been deleted.` })
-					await db.collection.updateOne({ serverName: 'Deep Sea Fishing' }, {
-						$pull: {
-							'merchChannel.components': thisSelection[0]
-						}
-					})
+					await errorChannel.send({ content: `A password was confirmed by <@!${cache.first().id}> and the message has been deleted.` })
 				} else {
-					interaction.followUp({ content: 'Thank you for responding.' })
-					buttonMessage.edit({ components: [] })
-					await db.collection.updateOne({ serverName: 'Deep Sea Fishing' }, {
-						$pull: {
-							'merchChannel.components': thisSelection[0]
-						}
-					})
+					await interaction.followUp({ content: 'Thank you for responding.' })
+					await buttonMessage.edit({ components: [] })
 				}
+				cache.delete(buttonMessage.id)
 			} catch (err) {
 				channels.errors.send(err)
 			}
