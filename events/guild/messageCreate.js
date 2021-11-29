76da91883c1694eab5c9543939c78c474a84cd13
Luke Hankey <68163698+LukeHankey@users.noreply.github.com>
2021-11-29 15:18:14 +0000

    Stop context menus from being ran

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -156,6 +156,7 @@ export default async (client, message) => {
 		}
 
 		try {
+			if (command?.menu === 'menu') return
 			command.guildSpecific === 'all' || command.guildSpecific.includes(message.channel.guild.id)
 				? command.run(client, message, args, perms, db)
 				: message.channel.send({ content: 'You cannot use that command in this server.' })
