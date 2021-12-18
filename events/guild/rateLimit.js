e3a02b8f7adef55e70b0ae8eec6cc55d26204495
Luke Hankey <68163698+LukeHankey@users.noreply.github.com>
2021-12-18 01:01:29 +0000

    Shoot rate limit event when > 1 min timeout

--- events/guild/rateLimit.js
+++ events/guild/rateLimit.js
@@ -2,5 +2,5 @@ export default async (client, rateLimitData) => {
 	const { timeout, limit, method, path, route, global } = rateLimitData
 	const errorsChannel = client.channels.cache.get('860930368994803732')
 
-	await errorsChannel.send({ content: `The bot is being rate limited:\nTimeout: ${timeout}}\nLimit: ${limit}\nMethod: ${method}\nPath: ${path}\nRoute: ${route}\nGlobal: ${global}` })
+	if (timeout > 60000) return	await errorsChannel.send({ content: `\`\`\`The bot is being rate limited:\nTimeout: ${timeout}\nLimit: ${limit}\nMethod: ${method.toUpperCase()}\nPath: ${path}\nRoute: ${route}\nGlobal: ${global}\`\`\`` })
 }
