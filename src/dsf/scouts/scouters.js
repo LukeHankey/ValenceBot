d78c1f104dd45f058ff095d4b02481d2b56b97b3
Luke Hankey <lukehankey@gmail.com>
2022-05-17 22:37:37 +0100

    feat: Add splitMessage utility function

--- src/dsf/scouts/scouters.js
+++ src/dsf/scouts/scouters.js
@@ -1,5 +1,6 @@
 import { ScouterCheck } from '../../classes.js'
-import { Util, Formatters } from 'discord.js'
+import { Formatters } from 'discord.js'
+import { splitMessage } from '../../functions.js'
 const scout = new ScouterCheck('Scouter')
 const vScout = new ScouterCheck('Verified Scouter')
 
@@ -59,7 +60,7 @@ const removeInactives = async (name, db, scoutTracker) => {
 		}
 	})
 	if (allItems.length) {
-		const split = Util.splitMessage(`${allItems.join('\n')}`)
+		const split = splitMessage(`${allItems.join('\n')}`)
 		return split.forEach(async content => channels.logs.send(`${removed.length} profiles removed.\n${Formatters.codeBlock(content)}`))
 	}
 }
