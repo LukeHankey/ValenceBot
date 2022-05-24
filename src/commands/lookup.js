ad16318fd060971ef346dd04b8b76aa9388ee4b5
Luke Hankey <l.hankey@oxcan.org>
2022-05-24 20:15:20 +0100

    feat: Show warning count in lookup

--- src/commands/lookup.js
+++ src/commands/lookup.js
@@ -28,11 +28,16 @@ export default {
 
 		const user = await db.collection.findOne({ userID: args[0] }, { projection: { _id: 0 } })
 		if (!user) return message.channel.send({ content: `<@!${args[0]}> is not in the database.` })
+		console.log(user)
+
+		if (!user.warnings){
+			user.warnings = []
+		}
 
 		const allData = await db.collection.countDocuments({})
 		const embedFields = [{
 			name: '\u200b',
-			value: `**firstTimestamp:** ${user.firstTimestamp}\n**firstTimestampReadable:** ${date(user.firstTimestampReadable)}\n**lastTimestamp:** ${user.lastTimestamp}\n**lastTimestampReadable:** ${date(user.lastTimestampReadable)}\n**Merch count:** ${user.count}\n**Other count:** ${user.otherCount}\n**Active for:** ${ms(user.lastTimestamp - user.firstTimestamp)}\n**Active:** ${user.active ? 'True' : 'False'}`,
+			value: `**firstTimestamp:** ${user.firstTimestamp}\n**firstTimestampReadable:** ${date(user.firstTimestampReadable)}\n**lastTimestamp:** ${user.lastTimestamp}\n**lastTimestampReadable:** ${date(user.lastTimestampReadable)}\n**Merch count:** ${user.count}\n**Other count:** ${user.otherCount}\n**Active for:** ${ms(user.lastTimestamp - user.firstTimestamp)}\n**Active:** ${user.active ? 'True' : 'False'}\n**Warnings:** ${user.warnings.length}`,
 			inline: true
 		}]
 
