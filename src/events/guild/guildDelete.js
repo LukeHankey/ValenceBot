ebc0bbef3b47d5f66b455136482a86e9a5cac855
Luke Hankey <l.hankey@oxcan.org>
2022-08-24 22:51:24 +0100

    Remove guildowner fetch on guildDelete

--- src/events/guild/guildDelete.js
+++ src/events/guild/guildDelete.js
@@ -5,12 +5,10 @@ export default async (client, guild) => {
 	const channels = await db.channels
 	try {
 		await db.collection.deleteOne({ _id: `${guild.id}` })
-		const owner = await guild.fetchOwner()
 
 		channels.logs.send(`The bot has left **${guild.name}**. The bot is in a total of ${client.guilds.cache.size} servers.
     \n\`\`\`diff\n+ Server name: ${guild.name}
 + Server ID: ${guild.id}
-+ Owner: ${owner.nickname}
 + Channel count: ${guild.channels.cache.size}
 + Member count: ${guild.memberCount}\`\`\``)
 	} catch (err) {
