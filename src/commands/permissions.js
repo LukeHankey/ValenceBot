2160609aa6f83394dfaa9043af2fdc8b992c25ed
Luke Hankey <l.hankey@oxcan.org>
2022-09-07 17:47:55 +0100

    Add channel type to ApplicationCommandPermissions switch

--- src/commands/permissions.js
+++ src/commands/permissions.js
@@ -13,46 +13,6 @@ export default {
 		name: 'permissions',
 		description: 'Assigns a role or user permissions to use a command.',
 		options: [
-			// {
-			// 	type: 1,
-			// 	name: 'set',
-			// 	description: 'Add or remove permissions for a user or role to a slash command.',
-			// 	options: [
-			// 		{
-			// 			type: 3,
-			// 			name: 'command',
-			// 			description: 'The name of the command to add permissions to.',
-			// 			required: true,
-			// 			autocomplete: true
-			// 		},
-			// 		{
-			// 			type: 9,
-			// 			name: 'mention',
-			// 			description: 'The role or user to add to the permissions.',
-			// 			required: true
-			// 		},
-			// 		{
-			// 			type: 3,
-			// 			name: 'type',
-			// 			description: 'The type of setting.',
-			// 			required: true,
-			// 			choices: [{
-			// 				name: 'Add',
-			// 				value: 'Add'
-			// 			},
-			// 			{
-			// 				name: 'Remove',
-			// 				value: 'Remove'
-			// 			}]
-			// 		},
-			// 		{
-			// 			type: 5,
-			// 			name: 'value',
-			// 			description: 'Allow or deny the role/user permission to use the command.',
-			// 			required: true
-			// 		}
-			// 	]
-			// },
 			{
 				type: 1,
 				name: 'for',
@@ -95,67 +55,6 @@ export default {
 		if (!commandArray.includes(commandName)) return interaction.reply({ content: `There is no command by that name. Try one of: \`${commandArray.join(', ')}\``, ephemeral: true })
 
 		switch (subType) {
-		// case 'set': {
-		// 	const userOrRoleId = interaction.options.getMentionable('mention').id
-		// 	const userOrRole = interaction.options.getMentionable('mention') instanceof GuildMember ? ApplicationCommandPermissionType.User : ApplicationCommandPermissionType.Role
-		// 	const value = interaction.options.getBoolean('value')
-		// 	const permType = interaction.options.getString('type')
-		// 	let guildPerms
-		// 	if (guild) guildPerms = await interaction.client.guilds.cache.get(interaction.guild.id)?.commands.fetch(guildCommand.first().permissions.commandId)
-
-		// 	switch (permType) {
-		// 	case 'Add':
-		// 		if (guild) {
-		// 			await guildPerms.permissions.add({
-		// 				permissions: [
-		// 					{
-		// 						id: userOrRoleId,
-		// 						type: userOrRole,
-		// 						permission: value
-		// 					}
-		// 				]
-		// 			})
-		// 		} else {
-		// 			await interaction.guild.commands.permissions.add({
-		// 				command: globalCommands.first().permissions.commandId,
-		// 				permissions: [
-		// 					{
-		// 						id: userOrRoleId,
-		// 						type: userOrRole,
-		// 						permission: value
-		// 					}
-		// 				]
-		// 			})
-		// 		}
-		// 		interaction.reply({ content: 'Permissions have been set.', ephemeral: true })
-		// 		break
-		// 	case 'Remove':
-		// 		if (guild && userOrRole === 'ROLE') {
-		// 			await guildPerms.permissions.remove({
-		// 				command: guildCommand.first().permissions.commandId,
-		// 				roles: [userOrRoleId]
-		// 			})
-		// 		} else if (guild && userOrRole === 'USER') {
-		// 			await guildPerms.permissions.remove({
-		// 				command: guildCommand.first().permissions.commandId,
-		// 				users: [userOrRoleId]
-		// 			})
-		// 		} else if (userOrRole === 'ROLE') {
-		// 			await interaction.guild.commands.permissions.remove({
-		// 				command: globalCommands.first().permissions.commandId,
-		// 				roles: [userOrRoleId]
-		// 			})
-		// 		} else {
-		// 			await interaction.guild.commands.permissions.remove({
-		// 				command: globalCommands.first().permissions.commandId,
-		// 				users: [userOrRoleId]
-		// 			})
-		// 		}
-		// 		interaction.reply({ content: 'Permissions have been set.', ephemeral: true })
-		// 		break
-		// 	}
-		// }
-		// 	break
 		case 'for': {
 			const cmd = guildCommand.filter(com => com.name === commandName)
 			const globCommands = await interaction.client.application?.commands.fetch()
@@ -164,10 +63,17 @@ export default {
 			const displayPerms = (perms) => {
 				const permList = perms.map(p => {
 					const suffix = '>'
-					let prefix = '<@!'
-					if (p.type === ApplicationCommandPermissionType.Role) prefix = '<@&'
+					let prefix = null
+					let permissionType = null
+					switch (p.type) {
+					case ApplicationCommandPermissionType.Role: prefix = '<@&'; permissionType = 'Role'
+						break
+					case ApplicationCommandPermissionType.User: prefix = '<@!'; permissionType = 'User'
+						break
+					case ApplicationCommandPermissionType.Channel: prefix = '<#'; permissionType = 'Channel'
+					}
 
-					return { name: `Type: ${prefix.includes('&') ? 'Role' : 'User'}`, value: `${prefix}${p.id}${suffix}\nAccess: ${p.permission ? '✅' : '❌'}`, inline: true }
+					return { name: `Type: ${permissionType}`, value: `${prefix}${p.id}${suffix}\nAccess: ${p.permission ? '✅' : '❌'}`, inline: true }
 				})
 
 				const permsEmbed = new EmbedBuilder()
