22c0ef08c4cabf6d531e57e0c41e1167d429db71
Luke Hankey <l.hankey@oxcan.org>
2022-09-10 02:27:26 +0100

    menu taking too long

--- src/handlers/interactions/contextMenu.js
+++ src/handlers/interactions/contextMenu.js
@@ -1,34 +1,36 @@
 export const contextMenu = async (interaction, db, data) => {
 	const client = interaction.client
 	const channels = await db.channels
+	const dsfServerErrorChannel = client.channels.cache.get('884076361940078682')
 	switch (interaction.commandName) {
 	case 'Mark event as dead.':
 		if ([data.merchChannel.channelID, data.merchChannel.otherChannelID].includes(interaction.channel.id)) {
 			try {
-				const dsfServerErrorChannel = client.channels.cache.get('884076361940078682')
+				console.log(interaction.targetId)
 				const message = await interaction.channel.messages.fetch(interaction.targetId)
+				console.log(message.content)
 				// 1st safeguard to check the cache.
-				if (message.reactions.cache.has('☠️')) return await interaction.editReply({ content: 'This call is already marked as dead.' })
+				if (message.reactions.cache.has('☠️')) return await interaction.editReply({ content: 'This call is already marked as dead.', ephemeral: true })
 
 				const reaction = await message.react('☠️')
 				const userReactCollection = await reaction.users.fetch()
 				const timestamp = interaction.createdAt.toString().split(' ').slice(0, 5).join(' ')
 
 				if (userReactCollection.size > 1) {
-					return await interaction.editReply({ content: 'This call is already marked as dead.' })
+					return await interaction.editReply({ content: 'This call is already marked as dead.', ephemeral: true })
 				}
 
-				await interaction.editReply({ content: 'Thank you for marking this call as dead.' })
-				dsfServerErrorChannel.send({ content: `\`\`\`diff\n\n+ Reaction Added by ${interaction.member.displayName} - Content: ${message.content}\n- User ID: ${interaction.member.id}\n- Timestamp: ${timestamp}\`\`\`` })
+				await interaction.editReply({ content: 'Thank you for marking this call as dead.', ephemeral: true })
+				dsfServerErrorChannel.send({ content: `\`\`\`diff\n\n+ Reaction Added by ${interaction.member.displayName} - Content: ${message.content}\n- User ID: ${interaction.member.id}\n- Timestamp: ${timestamp}\`\`\``, ephemeral: true })
 			} catch (err) {
 				if (err.code === 50001) {
 					// Missing Access
-					return await interaction.editReply({ content: 'I am not able to access this channel.' })
+					return await interaction.editReply({ content: 'I am not able to access this channel.', ephemeral: true })
 				}
 				channels.errors.send(err)
 			}
 		} else {
-			interaction.editReply({ content: 'You can\'t use that in this channel.' })
+			interaction.editReply({ content: 'You can\'t use that in this channel.', ephemeral: true })
 		}
 		break
 	case 'Affiliate Events': {
