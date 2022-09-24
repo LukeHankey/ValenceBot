492f08cbf0cdf802372f992f1a3e4b10753e4b95
Luke Hankey <lukehankey@gmail.com>
2022-09-24 17:33:48 +0100

    Comment out FSW until worlds are known

--- src/dsf/merch/main.js
+++ src/dsf/merch/main.js
@@ -2,7 +2,8 @@ import { MongoCollection } from '../../DataBase.js'
 import { merchRegex, otherCalls } from './constants.js'
 import { arrIncludesString, alreadyCalled } from './merchFunctions.js'
 import { addMerchCount, skullTimer, addOtherCount, otherTimer } from '../index.js'
-import { worldReaction, worlds } from './worlds.js'
+import { worldReaction } from './worlds.js'
+// import { worldReaction, worlds } from './worlds.js'
 
 const dsf = async (client, message, db) => {
 	const channels = await db.channels
@@ -32,11 +33,11 @@ const dsf = async (client, message, db) => {
 			alreadyCalled(message, messages)
 		) {
 			const worldNumber = parseInt(/\w(\d{1,3})/.exec(message.content)[1])
-			const freshStartWorlds = worlds.map((item) => (item.reason === 'fsw' ? item.world : null)).filter(Boolean)
-			let rolePing = '<@&670842187461820436>'
-			if (freshStartWorlds.includes(worldNumber)) {
-				rolePing = '<@&1022966016604651611>'
-			}
+			// const freshStartWorlds = worlds.map((item) => (item.reason === 'fsw' ? item.world : null)).filter(Boolean)
+			const rolePing = '<@&670842187461820436>'
+			// if (freshStartWorlds.includes(worldNumber)) {
+			// 	rolePing = '<@&1022966016604651611>'
+			// }
 
 			const sentMessage = await message.channel.send(`${rolePing} - ${message.content}`)
 			try {
