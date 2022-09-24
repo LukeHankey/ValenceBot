b4b1b6488bccac98875a7eba3225be43d765d61e
Luke <68163698+LukeHankey@users.noreply.github.com>
2022-09-24 17:42:49 +0100

    feat: Add world reactions (#68)
    * feat: Add world reactions
    
    Worlds that are difficult to get into or are annoying to hop to are added with reactions for an at glace look if users can get into those worlds
    
    * Refactor into own function to use in both merch and other calls
    
    * Comment out FSW until worlds are known
    
    * Different lag reaction

--- src/dsf/merch/main.js
+++ src/dsf/merch/main.js
@@ -2,6 +2,8 @@ import { MongoCollection } from '../../DataBase.js'
 import { merchRegex, otherCalls } from './constants.js'
 import { arrIncludesString, alreadyCalled } from './merchFunctions.js'
 import { addMerchCount, skullTimer, addOtherCount, otherTimer } from '../index.js'
+import { worldReaction } from './worlds.js'
+// import { worldReaction, worlds } from './worlds.js'
 
 const dsf = async (client, message, db) => {
 	const channels = await db.channels
@@ -25,22 +27,32 @@ const dsf = async (client, message, db) => {
 	if (message.author.bot) return
 	if (message.channel.id === channelID) {
 		await addMerchCount(client, message, db, scouters)
-		merchRegex.test(message.content) &&
-		arrIncludesString(disallowedWords, message.content) &&
-		alreadyCalled(message, messages)
-			? message.channel
-					.send(`<@&670842187461820436> - ${message.content}`)
-					.then(async (mes) => {
-						return setTimeout(() => mes.delete(), 200)
-					})
-					.catch(async (err) => {
-						const messageID = err.url.split('/')
-						return await message.channel.messages
-							.fetch(messageID[8])
-							.then((x) => x.delete())
-							.catch(async (err) => channels.errors.send(err))
-					})
-			: setTimeout(() => message.delete(), 200)
+		if (
+			merchRegex.test(message.content) &&
+			arrIncludesString(disallowedWords, message.content) &&
+			alreadyCalled(message, messages)
+		) {
+			const worldNumber = parseInt(/\w(\d{1,3})/.exec(message.content)[1])
+			// const freshStartWorlds = worlds.map((item) => (item.reason === 'fsw' ? item.world : null)).filter(Boolean)
+			const rolePing = '<@&670842187461820436>'
+			// if (freshStartWorlds.includes(worldNumber)) {
+			// 	rolePing = '<@&1022966016604651611>'
+			// }
+
+			const sentMessage = await message.channel.send(`${rolePing} - ${message.content}`)
+			try {
+				setTimeout(() => sentMessage.delete(), 200)
+			} catch (err) {
+				const messageID = err.url.split('/')
+				return await message.channel.messages
+					.fetch(messageID[8])
+					.then((x) => x.delete())
+					.catch((err) => channels.errors.send(err))
+			}
+			await worldReaction(worldNumber, message)
+		} else {
+			setTimeout(() => message.delete(), 200)
+		}
 		skullTimer(message, db)
 	} else if (message.channel.id === otherChannelID) {
 		await addOtherCount(client, message, db, scouters)
@@ -50,6 +62,9 @@ const dsf = async (client, message, db) => {
 			!alreadyCalled(message, otherMessages)
 		) {
 			return setTimeout(() => message.delete(), 200)
+		} else {
+			const worldNumber = parseInt(/\w(\d{1,3})/.exec(message.content)[1])
+			await worldReaction(worldNumber, message)
 		}
 		otherTimer(message, db)
 	}
