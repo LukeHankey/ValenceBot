1cedd80fa9f771fb7243a663d87fd10a784e033e
Luke Hankey <lukehankey@gmail.com>
2023-01-15 14:08:01 +0000

    No longer send messages in threads, just join them

--- src/events/guild/threadCreate.js
+++ src/events/guild/threadCreate.js
@@ -1,14 +1,4 @@
 export default async (_, thread) => {
+	// Just join all threads
 	await thread.join()
-	if (thread.sendable) {
-		if (thread.parentId === '938566070569144371' || thread.parentId === '630418192451371018') {
-			return
-		}
-		const archiveTime = thread.autoArchiveDuration
-		return thread.send({
-			content: `Hello! This thread: \`${thread.name}\` will be automatically archived in \`${
-				archiveTime / 60
-			} hours\` if there is no activity.`
-		})
-	}
 }
