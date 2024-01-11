bb7be9fb0db73c56d11fc8a9bd7de9a366907de7
Luke Hankey <lukehankey@gmail.com>
2024-01-11 11:59:18 +0000

    Add the database to the discord client

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import cron from 'node-cron'
 import { Client, Collection } from 'discord.js'
-import { MongoCollection } from './DataBase.js'
+import { MongoDataBase } from './DataBase.js'
 import { getData, addActive } from './scheduler/clan.js'
 import { Load } from './handlers/index.js'
 import { logger } from './logging.js'
@@ -26,19 +26,20 @@ process.on('unhandledRejection', (reason, p) => {
 	logger.error(`Unhandled Rejection at: ${p}\nreason: ${reason}`)
 })
 
-const db = new MongoCollection('Users')
+const database = new MongoDataBase('Members')
+client.database = database
 
 client.login(process.env.NODE_ENV === 'DEV' ? process.env.DEVELOPMENT_BOT : process.env.BOT_TOKEN)
 
 // Daily at 5am
 cron.schedule('0 5 * * *', async () => {
 	logger.info('running')
-	await addActive(db)
+	await addActive(client.database.users)
 })
 
 // Daily at 10am
 cron.schedule('0 10 * * *', async () => {
-	await getData(db)
+	await getData(client.database.users)
 })
 
 export default client
