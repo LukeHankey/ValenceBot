47122d2b397892e34395282d97a0cb355342339e
Luke Hankey <l.hankey@oxcan.org>
2025-05-11 17:47:37 +0100

    Edit webhook events with timer content

--- src/dsf/calls/eventTimers.js
+++ src/dsf/calls/eventTimers.js
@@ -1,5 +1,6 @@
 import { skullTimer, removeReactPermissions } from './skullTimer.js'
 import { setTimeout as delay } from 'timers/promises'
+import axios from 'axios'
 
 export const activeTimers = new Map()
 
@@ -26,7 +27,8 @@ export async function startEventTimer({ client, message, eventId, channelName, d
 		client,
 		message,
 		channelName,
-		database
+		database,
+		mistyUpdated: false
 	})
 
 	await timeout
@@ -34,17 +36,19 @@ export async function startEventTimer({ client, message, eventId, channelName, d
 }
 
 function updateMessageTimestamp(content, currentDurationMs, newDurationMs) {
-	const match = content.match(/<t:(\d+):R>/)
-	if (!match) return content // No timestamp found, return as-is
+	if (newDurationMs > 0) {
+		const match = content.match(/<t:(\d+):R>/)
+		if (!match) return content // No timestamp found, return as-is
 
-	const originalTimestamp = parseInt(match[1], 10)
-
-	const adjustedTimestamp = parseInt((originalTimestamp - currentDurationMs / 1000 + newDurationMs / 1000).toString())
-
-	return content.replace(/<t:\d+:R>/, `<t:${adjustedTimestamp}:R>`)
+		const originalTimestamp = parseInt(match[1], 10)
+		const adjustedTimestamp = parseInt((originalTimestamp - currentDurationMs / 1000 + newDurationMs / 1000).toString())
+		return content.replace(/<t:\d+:R>/, `<t:${adjustedTimestamp}:R>`)
+	} else {
+		return content.replace(/\s\|.*/, '')
+	}
 }
 
-export async function overrideEventTimer(eventId, newDurationMs) {
+export async function overrideEventTimer(eventId, newDurationMs, mistyUpdate = false) {
 	const current = activeTimers.get(String(eventId))
 	if (!current) return
 
@@ -65,14 +69,35 @@ export async function overrideEventTimer(eventId, newDurationMs) {
 			if (err.name !== 'AbortError') console.error(`[${eventId}] ⛔ Updated timer aborted`)
 		})
 
-	const content = current.message.content
-	await current.message.edit({ content: updateMessageTimestamp(content, current.durationMs, newDurationMs) })
+	const message = current.message
+	if (message.author.username === 'Alt1 Tracker' && !mistyUpdate) {
+		try {
+			const content = message.content
+			const updatedContent = updateMessageTimestamp(content, current.durationMs, newDurationMs)
+			const API_URL = process.env.NODE_ENV === 'DEV' ? 'http:localhost:8000' : 'https://api.dsfeventtracker.com'
+			const editWebhookResponse = await axios.patch(`${API_URL}/events/webhook/${message.id}`, {
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				content: updatedContent
+			})
+			if (editWebhookResponse.status !== 200) {
+				console.log('Did not receive the correct response')
+			} else {
+				console.log('Event editted successfully: ', editWebhookResponse)
+			}
+		} catch (err) {
+			console.error('Failed to edit the webhook', err.response.data.detail)
+		}
+	}
 
 	activeTimers.set(String(eventId), {
 		...current,
 		timeout,
 		abortController: controller,
 		startTime: Date.now(),
-		durationMs: newDurationMs
+		durationMs: newDurationMs,
+		message,
+		mistyUpdated: mistyUpdate
 	})
 }
